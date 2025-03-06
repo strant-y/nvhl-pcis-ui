a58d2ea5e8574796d5cd3d88d5d6854718bd3584
zhuzhibin <11127672329@qq.com>
2025-03-06 19:36:21 +0800

    清单信息回填

--- src/store/modules/data-opertaor.ts
+++ src/store/modules/data-opertaor.ts
@@ -91,6 +91,8 @@ export const dataOpertaor = defineStore(
                       srcTab='Term'
                   }else if(voNme =='payinfo'){
                       srcTab='Pay'
+                  }else if(voNme =='dist'){
+                      srcTab='DesignDist'
                   }
                   if (!!result['res']['composition'][srcTab] && result['res']['composition'][srcTab] instanceof Array && result['res']['composition'][srcTab].length > 0) { // 数组 并且很多行
                       const tab = k['pageType']; // 根据key获取tab 然后判断是否是GridEdit或FreeEdit
