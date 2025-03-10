fc573457d2218101029c52fd652b2574f5a829cd
chenzhenghui <zhchenq@isoftstone.com>
2025-03-10 19:05:47 +0800

    1.解决弹窗报错的问题

--- src/store/modules/data-opertaor.ts
+++ src/store/modules/data-opertaor.ts
@@ -51,7 +51,7 @@ export const dataOpertaor = defineStore(
 
     const setDataAll = (alldata: any) => {
       Object.keys(alldata).forEach((key) => {
-        if (tableRefs[key]) {
+        if (tableRefs[key] && tableRefs[key].setFormValue) {
           tableRefs[key].setFormValue(alldata[key]);
         }
       });
