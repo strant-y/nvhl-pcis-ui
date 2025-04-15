383fca7332a36aacd3f6c03059df3921da304838
zhuzhibin <11127672329@qq.com>
2025-04-15 09:27:09 +0800

    批改保费计算报错修改

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -877,7 +877,9 @@ const loadAppPlyInfo = (CAppNo) => {
             res["res"]["composition"]["EdrBase"][0]["EdrBase.cRatioTyp"]= '1';
             res["res"]["composition"]["EdrBase"][0]["EdrBase.cEdrType"]= props.param["cEdrType"];
             res["res"]["composition"]["EdrBase"][0]["EdrBase.cEdrRsnBundleCde"]= props.param["cRsnCde"];
-            res["res"]["composition"]["EdrBase"][0]["EdrBase.cEdrRsnDetail"]=''
+            if (props.param.cEdrType != "1") {
+                res["res"]["composition"]["EdrBase"][0]["EdrBase.cEdrRsnDetail"]=''
+            }
         }
         edrbase.value?.setFormValue(EdrBaseData);
       }
@@ -1065,8 +1067,10 @@ const calcPremiumEdr = () => {
   res["plyBase"]["Base.cDptCde"] = props.param.cDptCde;
   res["plyBase"]["Base.cProdNo"] = props.param.cProdNo;
   res["EdrBase"] = edrbase.value?.getFromValue();
-  res["EdrBase"]["EdrBase.cEdrRsnDetail"] =
-    res["EdrBase"]["EdrBase.cEdrRsnDetail"].join();
+  if(res["EdrBase"]["EdrBase.cEdrRsnDetail"]!=null&&res["EdrBase"]["EdrBase.cEdrRsnDetail"]!=''){
+      res["EdrBase"]["EdrBase.cEdrRsnDetail"] =
+        res["EdrBase"]["EdrBase.cEdrRsnDetail"].join();
+  }
   console.log(res);
   calcEdr(res).then((res) => {
     btn.loading = false;
