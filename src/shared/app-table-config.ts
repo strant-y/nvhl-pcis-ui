a2b076b5f91b3ba113650b71c604d99d69a13fde
chenzhenghui <zhchenq@isoftstone.com>
2025-04-17 09:19:08 +0800

    1.增加app-table数据修改接口方法

--- src/shared/app-table-config.ts
+++ src/shared/app-table-config.ts
@@ -33,6 +33,7 @@ export interface AppTableMethod {
   getPartnerPage: (flag?: boolean) => any;
   getFromValue: () => any;
   setFormSchema: (rowId: string, props: any, schama: any, value: any) => void;
+  setValueByRowKey:(props:string ,rowId: any, value:any) => void;
 }
 export interface MyTableMethod {
   addRow: (arg: any) => any;
