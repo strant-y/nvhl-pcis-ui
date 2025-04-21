3d16d1fea760f3b72bccb2e90160ce15cf7885aa
zhuzhibin <11127672329@qq.com>
2025-04-21 16:33:59 +0800

    投保单详情页无法加载数据

--- src/store/modules/data-opertaor.ts
+++ src/store/modules/data-opertaor.ts
@@ -265,7 +265,7 @@ export const dataOpertaor = defineStore(
                                 da = data[voNme];
                             }
                 }
-                res[voNme] = da;
+                res[voNme+'-ref'] = da;
             });
             return res;
         }
