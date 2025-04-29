a0579ef1f7c2449c037dd709018f92ed30203664
chenzhenghui <zhchenq@isoftstone.com>
2025-04-29 14:55:33 +0800

    1.解决街道未带入地址的问题

--- src/pcis/prodRef/applicantRef/applicant.vue
+++ src/pcis/prodRef/applicantRef/applicant.vue
@@ -510,8 +510,7 @@ function setregistAdd() {
     getAddressStr({ address: ads }).then((res: any) => {
     const { code, data, msg } = res;
     if (code === 200) {
-      const b = (data?data['addStr']:"" + a;
-      console.log(b);
+      const b = (data?data['addStr']:"") + a;
       setAddressStr("Applicant.cClntAddr", b);
     }
   });
