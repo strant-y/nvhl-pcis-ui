6d361eb9dfaa0378c048329b05700ed34dc2f95f
DESKTOP-LOGR02Q\issuser <zmj>
2025-05-14 14:53:50 +0800

    04产品清单功能

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -39,6 +39,7 @@ import { CardConfig, creatCardConfig } from "@/shared/mytemplate/card-config";
 import { dataOpertaor } from "@/store/modules/data-opertaor";
 import { DialogMethod } from "@/common/dzmodel/ComDialogConf";
 import { useRoute } from "vue-router";
+import { runInThisContext } from "vm";
 const route = useRoute();
 const dialog = ref<DialogMethod | null>(null);
 const opertaor = dataOpertaor();
@@ -69,16 +70,6 @@ let cComponentTableValue: string;
 const getCComponentTableValue = (cProdNo: string, title: string): string => {
   if (cProdNo == "040001") {
     return "AddressDist";
-  } else if (cProdNo == "043002") {
-    return "VehicleDist";
-  } else if (cProdNo == "043009") {
-    if (title == "实际用工地址/工程项目地址清单") {
-      return "ProjectDist";
-    } else if (title == "从业人员清单") {
-      return "EmployeeDist";
-    } else if (title == "从业人员清单汇总") {
-      return "DistSummary";
-    }
   } else if (cProdNo == "040002") {
     if (title == "雇员清单") {
       return "EmployeeDist";
@@ -88,6 +79,54 @@ const getCComponentTableValue = (cProdNo: string, title: string): string => {
       return "VehicleDist";
     } else if (title == "车辆清单汇总") {
       return "DistSummary";
+    }
+  } else if (cProdNo == "040003") {
+    if(title == "产品清单"){
+      return "ProductDist";
+    }else if(title =="销售区域清单"){
+      return "SalesDist";
+    }
+  } else if(cProdNo == "040005"){
+    if(title =="地址清单信息"){
+      return "AddressDist";
+    }else if(title == "人员清单"){
+      return "EducatorDist"
+    }
+  }else if(cProdNo == "040020"){
+      return "PersonnelDist";
+  }else if(cProdNo == "042001"){
+      return "DesignDist"
+  }else if(cProdNo == "042003"){
+      return "EducatorDist"
+  }else if(cProdNo == "043001"){
+    return "ElevatorDist";
+  } else if (cProdNo == "043002") {
+    return "VehicleDist";
+  } else if(cProdNo == "043007"){
+    return "VehicleDist";
+  }else if (cProdNo == "043009") {
+    if (title == "实际用工地址/工程项目地址清单") {
+      return "ProjectDist";
+    } else if (title == "从业人员清单") {
+      return "EmployeeDist";
+    } else if (title == "从业人员清单汇总") {
+      return "DistSummary";
+    }
+  }else if(cProdNo == "043010"){
+    return "EducatorDist"
+  } else if(cProdNo == "043013"){
+    return "PollutionDist"
+  }else if(cProdNo =="043020"){
+    if(title =="房屋清单"){
+      return "AddressDist"
+    }else if(title =="家庭成员清单"){
+      return "FamilyTgt"
+    }
+  }else if(cProdNo =="045001"){
+    if(title =="雇员清单信息"){
+      return "EmployeeDist"
+    }else if(title =="工程项目地址清单"){
+      return "ProjectDist"
     }
   }
   return "";
