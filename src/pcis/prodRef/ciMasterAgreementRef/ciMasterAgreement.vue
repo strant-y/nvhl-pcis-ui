7b9ffe8de209926fdc54673c94862e462d0694bb
DESKTOP-LOGR02Q\issuser <zmj>
2025-05-27 19:09:53 +0800

    联共保

--- src/pcis/prodRef/ciMasterAgreementRef/ciMasterAgreement.vue
+++ src/pcis/prodRef/ciMasterAgreementRef/ciMasterAgreement.vue
@@ -10,12 +10,30 @@ import {
 import { formInit } from "@/shared/from-init";
 import { dataOpertaor } from "@/store/modules/data-opertaor";
 const opertaor = dataOpertaor();
+import { useProductStore } from "@/store/modules/prod";
+const productStore = useProductStore();
 
 const props = defineProps({
   pageSchema: {
     type: [Object],
     required: true,
   },
+});
+
+// const nJiJntPrm = ref("0.00");   //联保总保费
+// const nJiJntAmt = ref("0.00");   //联保总保额
+// const nCiJntAmt = ref("0.00");   //共保总保额
+// const nCiJntPrm = ref("0.00");   //共保总保费
+
+// 响应式引用 store 中的值
+const nJiJntPrm = ref(productStore.nPrm);  // 联保总保费
+const nCiJntPrm = ref(productStore.nAmt);  // 共保总保费
+
+
+// 监听 store 中的变化并更新本地变量
+watchEffect(() => {
+  nJiJntPrm.value = productStore.nPrm;
+  nCiJntPrm.value = productStore.nAmt;
 });
 
 const tgtobjEditRef = ref<AppFreeEditMethod | null>(null);
@@ -29,10 +47,13 @@ onMounted(() => {
     exRules
   );
   Object.assign(formconfig1, formconfig11);
+  nextTick(() => {
+  });
 });
 
 // 绑定方法
 const method = {
+
   // func demo
   func1: () => {},
 };
