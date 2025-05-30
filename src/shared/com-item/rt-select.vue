c2cf7f146d5a606acbbc638b3f185352fb6cf898
chenzhenghui <zhchenq@isoftstone.com>
2025-05-30 16:29:40 +0800

    1.解决禁用状态下,初始化属性误加载的问题

--- src/shared/com-item/rt-select.vue
+++ src/shared/com-item/rt-select.vue
@@ -160,7 +160,7 @@ watch([() => props.modelValue], ([newModelValue]) => {
   selectedValue.value = newModelValue;
   if (props.item.typeCode && options.value.length === 0) {
     uploadOption();
-  }else if(props.item.disabled){  // 如果是禁用项,则固定刷新下拉选
+  }else if(props.item.typeCode && props.item.disabled){  // 如果是禁用项,则固定刷新下拉选
     uploadOption();
   }
 });
