8b27734cc3762ea77acf21f975607caf5c87e332
xq <xqke@isoftstone.com>
2025-06-03 18:51:58 +0800

    免赔条件 选择、修改、新增 功能

---
+++ src/pcis/prodRef/commodityRef/DeductibleFix.vue
@@ -0,0 +1,210 @@
+<template>
+  <div>
+    <el-tabs v-model="activeName" type="card" class="demo-tabs">
+      <el-tab-pane label="添加免赔条件" name="first">
+        <div class="totalBox">已选择 {{ selected.length }} 项</div>
+        <el-table
+          ref="multipleTableRef"
+          :data="pageresult.list"
+          style="width: 100%"
+          :row-class-name="tableRowClassName"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column
+            type="selection"
+            :selectable="selectable"
+            width="55"
+          />
+          <el-table-column type="index" label="序号" width="55" />
+          <el-table-column label="是否可选" width="100">
+            <template #default="scope">
+              <el-tag type="primary">{{
+                scope.row["cIfMust"] == "1" ? "必选" : "可选"
+              }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column property="cDeductibleContent" label="免赔条件内容" />
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="添加其他免赔条件" name="second">
+        <el-table
+          ref="multipleTableRef"
+          :data="addTableData"
+          style="width: 100%"
+        >
+          <el-table-column property="addIndex" label="序号" width="55" />
+          <el-table-column property="cDeductibleContent" label="免赔内容">
+            <template #default="scope">
+              <el-input v-model="scope.row['cDeductibleContent']"></el-input>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button @click="add" class="addSty" :icon="Plus">新增一行</el-button>
+      </el-tab-pane>
+    </el-tabs>
+    <div class="btnSty">
+      <el-button @click="close">取消</el-button>
+      <el-button type="primary" @click="returnData">确定</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref, reactive, onMounted } from "vue";
+import { Plus } from "@element-plus/icons-vue";
+import { createFreeButtonBase } from "@/shared/button-config";
+import { DialogMethod } from "@/common/dzmodel/ComDialogConf";
+import {
+  AppTableConfig,
+  AppTableMethod,
+  createTableEditConfig,
+} from "@/shared/app-table-config";
+import { codeListViewStore } from "@/store";
+import { getPrdDeductible } from "@/api/prod";
+const codeListStore = codeListViewStore();
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+  method: {
+    type: Object,
+    default: () => {
+      return {};
+    },
+  },
+});
+const emits = defineEmits(["handleClose"]);
+const multipleTableRef = ref<MyTableMethod | null>(null);
+const selected = ref([]);
+const pageresult = reactive<Pageresult>({
+  /** 数据列表 */
+  list: [],
+});
+const addTableData = reactive([]); //添加其他特约
+const activeName = ref("first");
+const selectable = (row) => row["cIfMust"] != "1"; //这里调用是把必选的置灰
+const tableRowClassName = ({ row, rowIndex }) => {
+  let sty = "";
+  selected.value.forEach((item) => {
+    if (item["cPkId"] == row["cPkId"]) {
+      sty = "checkedSty";
+    }
+  });
+  return sty;
+};
+const handleSelectionChange = (selection) => {
+  selected.value = selection;
+};
+
+const refreshData = () => {
+  const param = {
+    cProdNo: props.data.cProdNo,
+    pageNum: 1,
+    pageSize: 999,
+  }
+  console.log('param', param)
+  // 查询列表数据
+  getPrdDeductible(param).then((res) => {
+    if (res.data.result) {
+      console.log("res.data.result-2", res.data.result);
+      pageresult.list = [];
+      res.data.result.forEach((item, index) => {
+        pageresult.list.push({
+          cPkId: item.cPkId,
+          cDeductibleContent: item.cDeductibleContent,
+          cStatus: item.cStatus, //是否必选
+          cIfMust: item.cIfMust, //是否必选
+          cIfEdit: item.cIfEdit, //是否可修改
+          cIfFix: "1", 
+        });
+      });
+      nextTick(() => {
+        toggleSpecificRow(); //这里调用是把必选的选中
+        setSelected();
+      });
+    }
+  });
+};
+
+// 切换指定行的选中状态
+const toggleSpecificRow = () => {
+  if (multipleTableRef.value) {
+    // 假设要切换 id 为 2 的行的选中状态
+    pageresult.list.forEach((row) => {
+      if (row["cIfMust"] == "1") {
+        multipleTableRef.value.toggleRowSelection(row, true);
+      }
+    });
+  }
+};
+
+function add() {
+  addTableData.push({
+    addIndex: addTableData.length + 1, //序号
+    cPkId: "", 
+    cDeductibleContent: "",
+    cStatus: "",
+    cIfEdit: "1", // 是否可修改
+    cIfMust: "", // 是否必选
+    cYuliu1: "",
+  });
+}
+
+//点击确定按钮时把选中的数据派发给父组件
+const returnData = () => {
+  if (activeName.value == "first") {
+    let tempData = multipleTableRef.value.getSelectionRows();
+    props.method.getSelected(tempData);
+  } else {
+    console.log("addTableData", addTableData);
+    props.method.getSelected(addTableData);
+  }
+  close();
+};
+const close = () => {
+  emits("handleClose");
+};
+
+function setSelected() {
+  const lastSelected = props.data.selectedData;
+  console.log("lastSelected", lastSelected);
+  if (lastSelected && lastSelected.length) {
+    lastSelected.forEach((item) => {
+      pageresult.list.forEach((item2) => {
+        if (item["cPkId"] === item2["cPkId"]) {
+          item2["checked"] = true;
+        }
+      });
+    });
+  }
+}
+
+onMounted(() => {
+  refreshData();
+});
+</script>
+
+<style scoped lang="scss">
+.btnSty {
+  text-align: right;
+  margin-top: 10px;
+}
+.totalBox {
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  padding-left: 20px;
+  background: #fefce7;
+  border: 1px solid #f3e4b9;
+  margin-bottom: 10px;
+}
+:deep .el-table .checkedSty {
+  background-color: #ffe8e6;
+}
+.addSty {
+  border: 1px dashed #ccc;
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
---
+++ src/pcis/prodRef/commodityRef/DeductibleFixEdit.vue
@@ -0,0 +1,121 @@
+<template>
+  <el-dialog v-model="dialogVisible" title="编辑免赔内容" width="80%">
+    <div class="form-item">
+      <div class="content-container">
+        <div
+          v-for="(item, index) in cNmeCnArray"
+          :key="index"
+          class="content-item"
+        >
+          <el-input
+            v-if="item === '**'"
+            v-model="inputValues[index]"
+            @input="updateCNmeCn(index, $event)"
+            :class="`input-${index}`"
+            placeholder="请输入"
+            size="small"
+            style="font-size: 13px;"
+          ></el-input>
+          <span v-else>{{ item }}</span>
+        </div>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleCancel" class="custom-button">取消</el-button>
+        <el-button type="primary" @click="handleSave" class="custom-button"
+          >保存</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch, onMounted, computed } from "vue";
+const dialogVisible = ref(true);
+const props = defineProps(["data", "callback"]);
+const rowData = ref(props.data);
+//rowData当前行数据
+/**
+ * 拿到特约内容字段，通过***分割为数组，然后在html部分直接循环该数组，
+ * 如果数组项为***则特换为input输入框，等用户输入完成后，再把数组join合并为实际的特约内容，回显到特约组件
+ *
+ *
+ */
+console.log("rowData", rowData);
+onMounted(() => {
+  console.log("cNmeCnArray", cNmeCnArray.value);
+});
+
+// 使用正则表达式分割字符串，保留分隔符 ** 作为单独的数组项
+const cNmeCnArray = computed(() => rowData.value.cDeductibleContent.split(/(\*+)/));
+// const inputValues = ref<string[]>([]);
+const inputValues = ref(
+  cNmeCnArray.value.map((item) => (item === "**" ? "" : item))
+);
+
+const updateCNmeCn = (index: number, value: string) => {
+  inputValues.value[index] = value;
+};
+const handleCancel = () => {
+  dialogVisible.value = false;
+  props.callback({type: 'close'});
+};
+const handleSave = () => {
+  dialogVisible.value = false;
+  const parts = cNmeCnArray.value.map((item, idx) =>
+    item === "**" ? inputValues.value[idx] : item
+  );
+  rowData.value.cDeductibleContent = parts.join("");
+  props.callback({type: 'ok', data: rowData.value});
+};
+</script>
+<style scoped>
+.form-item {
+  margin-left: 30px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-size: 16px; /* 统一字体大小 */
+}
+
+.el-row {
+  display: flex;
+  align-items: center;
+}
+
+.el-col {
+  display: flex;
+  align-items: center;
+}
+
+
+.dialog-footer {
+  text-align: right;
+  margin-top: 20px;
+}
+
+.custom-button {
+  font-size: 16px; /* 统一按钮字体大小 */
+}
+/* 为每个输入框添加不同的边框样式 */
+.input-0 .el-input__inner {
+  border-color: red; /* 示例颜色 */
+}
+
+.input-1 .el-input__inner {
+  border-color: blue; /* 示例颜色 */
+}
+.content-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.content-item {
+  display: inline-block;
+  margin-right: 5px; /* 调整间距 */
+  height: 30px;
+  display: flex;
+  align-items: end;
+}
+</style>
