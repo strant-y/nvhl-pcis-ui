e4cbce6ec53111a28c930065782cb322631fa5c0
wangxiran <xrwangda@isoftstone.com>
2025-06-12 21:07:42 +0800

    perf: 修改自定义出单模板出单时条款查询入参

--- src/views/pcis/guide/CustomRecording.vue
+++ src/views/pcis/guide/CustomRecording.vue
@@ -538,8 +538,12 @@ function handleRecordTypeChange(val:any) {
     loadOptions(2);
     labelNm.value = "方案";
     formconfig1.value.cIsPlan = '1';
-  } else {
+  } else if (val == "1") {
     loadOptions();
+    labelNm.value = "条款";
+    formconfig1.value.cIsPlan = '0';
+  } else {
+    loadOptions(3);
     labelNm.value = "条款";
     formconfig1.value.cIsPlan = '0';
   }
