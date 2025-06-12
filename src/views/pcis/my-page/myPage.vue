b865db0fc17abc87d0fb32f6bcb4b327e62a3247
zhangmingjie <zmj>
2025-06-12 10:39:06 +0800

    联共保出单费用默认值添加

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -1674,7 +1674,8 @@ const setCiInfo = (base: any) => {
   ci["Ci.nCiPrm"] = base["Base.nPrm"];
   ci["Ci.cChiefMrk"] = "1"
   ci["Ci.cIssueMrk"] = "1"
-  
+  ci["Ci.nPlyFee"]="0.00"
+  ci["Ci.nPlyFeeRate"]="0.00"
   ci["Ci.cCoinsurerCde"] = "327001"
   ci["Ci.cSubDptCde"] = props.param.cDptCde
   ciList.push(ci)
