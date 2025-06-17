e39ef0ffb2908c9a846d49d383df477122b632b4
wangxiran <xrwangda@isoftstone.com>
2025-06-17 19:18:37 +0800

    perf: 方案配置中条款信息的保险费设置为可编辑状态

--- src/pcis/prodRef/cvrgRef/trem-template.vue
+++ src/pcis/prodRef/cvrgRef/trem-template.vue
@@ -619,6 +619,14 @@ function dataInit() {
     if (d.termTitleConf?.CCnm) {
       termTitleConf.value = JSON.parse(d.termTitleConf.CCnm);
     }
+    // 方案配置时条款信息中的保险费是可以编辑的
+    if(route.name === "plan-info") {
+      termFactormap.value.forEach((item:any) => {
+        if(item.prop === "Term.nInsuranceFee") {
+          item.disabled = false
+        }
+      })
+    }
     if (props.disabledFlag) {
       setDisabledAll();
     }
@@ -645,6 +653,14 @@ function dataInit() {
         terconfig.addConfig(queryKey, data.data);
       } else {
         ElMessage.error(msg);
+      }
+      // 方案配置时条款信息中的保险费是可以编辑的
+      if(route.name === "plan-info") {
+        termFactormap.value.forEach((item:any) => {
+          if(item.prop === "Term.nInsuranceFee") {
+            item.disabled = false
+          }
+        })
       }
       if (props.disabledFlag) {
         setDisabledAll();
@@ -883,10 +899,6 @@ function methodLink(items: any) {
     for (const i in items) {
       if (items[i]["func"] && typeof items[i]["func"] === "string") {
         items[i]["func"] = methodMap[items[i]["func"]];
-      }
-      // 方案配置时条款信息中的保险费是可以编辑的
-      if(items[i].prop === "Term.nInsuranceFee" && route.name === "plan-info") {
-        items[i].disabled = false
       }
     }
   }
