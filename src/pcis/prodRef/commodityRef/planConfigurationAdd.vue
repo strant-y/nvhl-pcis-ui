7b733a6f9a1ad89b1c5a47b77c834685229a5f91
p-ningwenchao <p-ningwenchao@xiaomi.com>
2025-06-23 14:18:15 +0800

    处理条款问题

--- src/pcis/prodRef/commodityRef/planConfigurationAdd.vue
+++ src/pcis/prodRef/commodityRef/planConfigurationAdd.vue
@@ -30,9 +30,11 @@ const dzmodal = useDzModal();
 const opertaor = dataOpertaor();
 const tabref = opertaor.getTableRefByKey("commodityBasicInfo");
 const tabProref = opertaor.getTableRefByKey("productBasicInfo");
+import { descryptParameter, encryptParameter } from "@/utils/encipher";
 const route = useRoute();
 const query = ref(route.query);
-const param = JSON.parse(query.value?.param ? String(query.value.param) : "{}");
+const param = JSON.parse(query.value?.param ? descryptParameter(query.value.param) : "{}"); 
+// const param = JSON.parse(query.value?.param ? String(query.value.param) : "{}"); 
 const emits = defineEmits(["ok", "cancel"]);
 import {
   AppFreeEditConfig,
