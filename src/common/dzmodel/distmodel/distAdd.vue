0591f8c809bb335c2cf1ad7e084499956053cce6
xq <xqke@isoftstone.com>
2025-06-24 18:20:46 +0800

    清单新增保存问题处理

--- src/common/dzmodel/distmodel/distAdd.vue
+++ src/common/dzmodel/distmodel/distAdd.vue
@@ -49,88 +49,8 @@ const props = defineProps({
     default: () => ({}),
   },
 });
-const getCComponentTable = () => {
-  // const cProdNo = route.params.param.cProdNo;
-  // if (cProdNo === "040001") return "AddressDist";
-  // if (cProdNo === "043002") return "VehicleDist";
-  // if (cProdNo === "040020") return "PersonnelDist";
-  // if (cProdNo === "042001") return "DesignDist";
-  // if (cProdNo === "042003") return "EducatorDist";
-  // if (cProdNo === "043001") return "ElevatorDist";
-  // if (cProdNo === "043007") return "VehicleDist";
-  // if (cProdNo === "043010") return "EducatorDist";
-  // if (cProdNo === "043013") return "PollutionDist";
-  // if (cProdNo === "043009") {
-  //   switch (props.data.tab) {
-  //     case "实际用工地址/工程项目地址清单":
-  //       return "ProjectDist";
-  //     case "从业人员清单":
-  //       return "EmployeeDist";
-  //     case "从业人员清单汇总":
-  //       return "DistSummary";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  // if (cProdNo === "040002") {
-  //   switch (props.data.tab) {
-  //     case "雇员清单":
-  //       return "EmployeeDist";
-  //     case "雇员清单汇总":
-  //       return "DistSummary";
-  //     case "车辆清单":
-  //       return "VehicleDist";
-  //     case "车辆清单汇总":
-  //       return "DistSummary";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  // if(cProdNo === "040003"){
-  //   switch(props.data.tab){
-  //     case "产品清单":
-  //       return "ProductDist";
-  //     case "销售区域清单":
-  //       return "SalesDist";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  // if(cProdNo === "040005"){
-  //   switch(props.data.tab){
-  //     case "地址清单信息":
-  //       return "AddressDist";
-  //     case "人员清单":
-  //       return "EducatorDist";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  // if(cProdNo === "043020"){
-  //   switch(props.data.tab){
-  //     case "房屋清单":
-  //       return "AddressDist";
-  //     case "家庭成员清单":
-  //       return "FamilyDist";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  // if(cProdNo === "045001"){
-  //   switch(props.data.tab){
-  //     case "雇员清单信息":
-  //       return "EmployeeDist";
-  //     case "工程项目地址清单":
-  //       return "ProjectDist";
-  //     default:
-  //       return "";
-  //   }
-  // }
-  return props.data.compKey ? props.data.compKey.substring(0, props.data.compKey.length - 6) : "";
-  // return "";
-};
-const cComponentTable = computed(() => getCComponentTable());
 
+const cComponentTable = computed(() => props.data.compKey ? props.data.compKey.replace(/\d+/g, '') : "");
 
 const dataParams = ref({});
 const appNo = ref("");
