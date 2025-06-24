2c915bf6c3ab293048559d8523768351551b5d15
xq <xqke@isoftstone.com>
2025-06-24 17:53:08 +0800

    清单查询参数、清单汇总关系、汇总查询参数 处理

--- src/api/prod/index.ts
+++ src/api/prod/index.ts
@@ -1243,6 +1243,10 @@ export function saveDist(queryParams: any): AxiosPromise<any> {
 export function selectDist(queryParams: any): AxiosPromise<any> {
   return post(`/policy/selectDist`, queryParams);
 }
+//查询清单对应汇总组件key
+export function distMapCollectCompKey(queryParams: any): AxiosPromise<any> {
+  return post(`/proddef/getDistSummaryComponentKeyByDistComponentKeyAndProdNo`, queryParams);
+}
 //删除清单
 export function deleteDist(queryParams: any): AxiosPromise<any> {
   return post(`/policy/deleteDist`, queryParams);
