4d983b4601f9612e385bf8845440a8bed5dda4e2
wangxiran <xrwangda@isoftstone.com>
2025-06-28 17:36:01 +0800

    feat: 增加根据code和val解析字典值接口

--- src/api/prod/index.ts
+++ src/api/prod/index.ts
@@ -1521,3 +1521,8 @@ export function exportRenewalInsurance(data:any) {
 export function getPolicy(queryParams: any): AxiosPromise<any> {
     return post(`/policy/getPolicy`, queryParams);
 }
+// 根据code和val解析字典值
+export function getEdrNmeByCde(queryParams: any): AxiosPromise<any> {
+    return post(`/edr/getNmeByCde`, queryParams);
+}
+
