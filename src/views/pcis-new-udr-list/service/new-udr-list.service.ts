ff704604faa1d65ce4c12f5962b0bcd967eaee00
wangxiran <xrwangda@isoftstone.com>
2025-07-02 14:27:22 +0800

    feat: 增加询价查询、根据询价单号查询基本信息接口

--- src/views/pcis-new-udr-list/service/new-udr-list.service.ts
+++ src/views/pcis-new-udr-list/service/new-udr-list.service.ts
@@ -396,6 +396,24 @@ export const NewUdrListService = () => {
     }
   }
 
+  // 询价查询（待核保）
+  const getInquiryNewUdrList = async (ops: any) => {
+    try {
+      return post(`underwriting/query/newUdrInquiryList`, ops);
+    } catch (error) {
+      return handleError(error);
+    }
+  }
+
+  // 询价查询根据询价单号查询基本信息
+  const getBaseInfoByInquiryNo = async (ops: any) => {
+    try {
+      return post(`policy/getBaseInfoByInquiryNo`, ops);
+    } catch (error) {
+      return handleError(error);
+    }
+  }
+
   return {
     getNewUdrList,
     getBackUdrList,
@@ -432,5 +450,7 @@ export const NewUdrListService = () => {
     saveRiskData,
     riskQueryData,
     queryComponentCodeList,
+    getInquiryNewUdrList,
+    getBaseInfoByInquiryNo,
   };
 }
