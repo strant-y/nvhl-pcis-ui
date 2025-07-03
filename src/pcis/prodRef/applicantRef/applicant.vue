7064c4a3b5b3151467f6be61c9b728096ecc02ad
ChenZhenghui <zhchenq@isoftstone.com>
2025-07-03 20:12:09 +0800

    1.投保人,是否绿色产业客户字段解禁

--- src/pcis/prodRef/applicantRef/applicant.vue
+++ src/pcis/prodRef/applicantRef/applicant.vue
@@ -107,8 +107,8 @@ onMounted(() => {
     setFormItem("Applicant.cFax", { rules: [getRules("faxNumber", {})] });
 
     
-    setFormItem("Applicant.cGreenIndustryCustomers",{disabled: true});
-    setFormItem("Applicant.cGreenIndustryList",{disabled: true});
+    // setFormItem("Applicant.cGreenIndustryCustomers",{disabled: true});
+    // setFormItem("Applicant.cGreenIndustryList",{disabled: true});
     setFormItem("Applicant.cGcidCode", {rules: [getRules("leiCode", {})]});
     // 关联交易审批单编号
     setFormItem("Applicant.cRelateNo", {rules: [getRules("txnApprovalNo", {})]});
