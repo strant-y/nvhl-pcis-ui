d4146758dbfbd0b0f9449c5e26c584dec2437d96
ChenZhenghui <zhchenq@isoftstone.com>
2025-07-03 20:22:29 +0800

    #1993 1.是否小微企业放开

--- src/pcis/prodRef/applicantRef/applicant.vue
+++ src/pcis/prodRef/applicantRef/applicant.vue
@@ -63,10 +63,10 @@ onMounted(() => {
   Object.assign(formconfig1, formconfig11);
   nextTick(() => {
     //是否小微企业，默认非必填、只读
-    setFormItem("Applicant.cIsMicroEntpris", {
-      rules: null,
-      disabled: true,
-    });
+    // setFormItem("Applicant.cIsMicroEntpris", {
+    //   rules: null,
+    //   disabled: true,
+    // });
     //【国民经济行业分类】初始化必填，只有法人时才必填，现在个人也是必填了（老系统需求：040001/042002/043004/043005/043011五款产品不区分法人个人投保，国民经济行业分类都必填，其他产品只有法人才必填）
     const cProdNo = param.cProdNo;
     if (
--- src/pcis/prodRef/insuredRef/insured.vue
+++ src/pcis/prodRef/insuredRef/insured.vue
@@ -66,10 +66,10 @@ onMounted(() => {
   Object.assign(formconfig1, formconfig11);
   nextTick(() => {
     //是否小微企业，默认非必填、只读
-    setFormItem("Insured.cIsMicroEntpris", {
-      rules: null,
-      disabled: true,
-    });
+    // setFormItem("Insured.cIsMicroEntpris", {
+    //   rules: null,
+    //   disabled: true,
+    // });
   });
   //【国民经济行业分类】初始化必填，只有法人时才必填，现在个人也是必填了（老系统需求：040001/042002/043004/043005/043011五款产品不区分法人个人投保，国民经济行业分类都必填，其他产品只有法人才必填）
   const cProdNo = route.params.param?.cProdNo;
