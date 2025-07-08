4bc269595cd0e6e71997dd8d4eabf31acb90134b
ChenZhenghui <zhchenq@isoftstone.com>
2025-07-08 10:56:22 +0800

    1.恢复表格,扩展列显示的问题

--- src/shared/com-item/rt-table.vue
+++ src/shared/com-item/rt-table.vue
@@ -34,7 +34,7 @@
       <el-table-column
         type="expand"
         :index="indexMethod"
-        v-show="item.showExpand ? item.showExpand : false"
+        v-if="item.showExpand ? item.showExpand : false"
         :align="item.align ? item.align : 'center'"
       >
         <template #default="props">
