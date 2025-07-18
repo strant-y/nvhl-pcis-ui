c6fb3592683a058fe76c1f86a203d82645b395af
wangxiran <xrwangda@isoftstone.com>
2025-07-18 16:40:23 +0800

    perf: 调整清单序号列的列宽

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -123,7 +123,12 @@ onMounted(async () => {
     }
     return e;
   });
-  tableconfig.value.fromSchema = formconfig1.value.fromSchema;
+  tableconfig.value.fromSchema = formconfig1.value.fromSchema.map((item:any) => {
+    if(item.prop === "Dist.nSeqNo" || item.title === "序号") {
+      item.width = 60
+    }
+    return item;
+  });
   tableconfig.value.formconfig = createAppGridEditConfig({
     titleBtns: formconfig1.value.titleBtns,
     fromSchema: formconfig1.value.distSchema,
