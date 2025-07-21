cac08861212b1c05ac7161c42079bea15fd66608
wangxiran <xrwangda@isoftstone.com>
2025-07-21 20:30:44 +0800

    fix: 清单导出按钮点击无反应

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -424,9 +424,6 @@ const method = {
   },
   //导出
   exportExcel: () => {
-
-    console.log(getTableData())
-    return false
     let paramitem  = Object.assign(formconfig1.value, {
       cComponentTable: cComponentTableValue,
     });
