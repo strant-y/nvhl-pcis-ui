04a2f72216d9f5105313ef3dd38c7b95b51b467c
zhangmingjie <zmj>
2025-07-21 08:05:05 +0800

    bug修复

--- src/pcis/prodRef/plyBaseRef/plybase.vue
+++ src/pcis/prodRef/plyBaseRef/plybase.vue
@@ -469,17 +469,28 @@ const method = {
                 false
               )
               .then((res) => {
-                if (res && res.code == 200) {
-                  const codeValData = res.data;
+                console.log("业务员返回值",res)
+                if (res) {
+                  const codeValData = res;
                   if (codeValData) {
                     setFormItem("Base.cIntroSalecde", {
                       loadData: codeValData,
                     });
+                    const ciRef = opertaor.getTableRefs()['ci'];
+                    if (!!ciRef) {
+                      ciRef.initcbusiner({
+                        cSlsId: params.CSlsCde, //业务员员工号
+                        cSlsNme: params.CSlsNme, //业务员名称
+                        loadData:codeValData,
+                      });
+                    }
+                    console.log("业务员下拉值",codeValData)
                     // 当选择了业务员时，服务机构业务员默认为业务员
                     setValue("Base.cIntroSalecde", params.CSlsCde);
                   }
                 }
               });
+              
             dialogRef.value?.handleClose();
           },
         },
