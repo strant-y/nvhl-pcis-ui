07ed90aa09536cbc60fa673233c880956b2be85d
p-ningwenchao <p-ningwenchao@xiaomi.com>
2025-07-23 20:24:06 +0800

    0723修复缺陷

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -211,7 +211,7 @@
                   : true
               "
             >
-          <!-- {{ k.pageKey }}  -->
+          <!-- {{ k.pageKey }} || {{ k.pageCode }} -->
               <component
                 v-if="currentIndex >= i"
                 :ref="
