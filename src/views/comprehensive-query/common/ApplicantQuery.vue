f7736d8d1ed3789e0a51161f565fc5302c84c207
wangxiran <xrwangda@isoftstone.com>
2025-07-25 11:55:37 +0800

    fix: 修复选择批单复制出单时页面不可编辑

--- src/views/comprehensive-query/common/ApplicantQuery.vue
+++ src/views/comprehensive-query/common/ApplicantQuery.vue
@@ -892,7 +892,7 @@ const tableObj = {
                         router.push({
                             path: "/pcis/my-page",
                             query: {
-                                param: JSON.stringify({ ...data, ...{ pageType: "copy" } }),
+                                param: JSON.stringify({ ...data, ...{ pageType: "copy", cAppTyp: 'A' } }),
                             },
                         });
                     } else {
