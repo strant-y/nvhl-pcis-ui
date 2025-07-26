71d24f285c37f3e5d2f74c188c3edb5f5d759de7
ChenZhenghui <zhchenq@isoftstone.com>
2025-07-26 15:48:05 +0800

    1.优化条款,删除按钮显示逻辑,针对增加条款,删除条款时,按钮的显示与控制

--- src/pcis/prodRef/cvrgRef/plyCvrg04.vue
+++ src/pcis/prodRef/cvrgRef/plyCvrg04.vue
@@ -23,7 +23,7 @@
                     :item="btnItem.addPlan"
                   />
                   <rt-button
-                    v-if="!btnItem.delPlan.hidden"
+                    v-if="showPlanDelete(k,btnItem.delPlan)"
                     @click="()=>{
                       if(Object.keys(planData).length === 1){
                         ElMessage.error('仅剩1个方案时,不能删除!');
@@ -417,6 +417,19 @@ const method = {
   },
 };
 
+/** 判断删除按钮是否显示 */
+function showPlanDelete(pl: any){
+  let r =  !btnItem.value.delPlan.hidden;
+  if(parparam.cRsnCde === '09'){    // 增加条款时,如果新增的条款, 可以正常显示删除按钮
+    if(planData.value[pl]['m']){
+      const rowId = planData.value[pl]['m'][0]['Term.cRowId'];
+      if(!rowId){     // 如果不存在rowId,则是新增条款,正常显示删除按钮
+        r = true;
+      }
+    }
+  }
+  return r;
+}
 function addPlanMethod() {
   let maxindex = 0;
     const l = Object.keys(planData.value).forEach((k: any) => {
@@ -547,11 +560,17 @@ function deletePlan(plan: string) {
     type: "warning",
   }).then(() => {
     if (parparam.cEdrType) {
-      Object.keys(planData.value[plan]).forEach((item) => {
-        for (let i = 0; i < planData.value[plan][item].length; i++) {
-          tremTemplateRefs.value[plan+item+i].setCancel();
-        }
-      });
+
+      const rowId = planData.value[plan]['m'][0]['Term.cRowId'];
+      if(!rowId){   //如果不存在行RowId,则是新增条款,直接删除即可
+        delete planData.value[plan];
+      }else{
+        Object.keys(planData.value[plan]).forEach((item) => {
+          for (let i = 0; i < planData.value[plan][item].length; i++) {
+            tremTemplateRefs.value[plan+item+i].setCancel();
+          }
+        });
+      }
     }else{
       delete planData.value[plan];
     }
