62b1805b07b7333e3493ffed2f46836a81476592
zhangmingjie\YAIC <mjzhangcf@isoftstone.com>
2025-07-28 17:37:45 +0800

    保费计算后联保机构回显成编码问题处理

--- src/pcis/prodRef/ciRef/ci.vue
+++ src/pcis/prodRef/ciRef/ci.vue
@@ -177,14 +177,17 @@ const method = {
             codeListParam: { "CParCde": "subdpt", cParCde: "327001" },
           })
           .then((res) => {
-            freeEditRef.value?.setRowFieldProp(
-                rowId,
-                "Ci.cSubDptCde",
-                "loadData",
-                res
-            );
+            freeEditRef.value?.addCodeListMap({
+              code:"Ci.cSubDptCde"+rowId,
+              list:res,
+            })
           });
     } else {
+      freeEditRef.value?.addCodeListMap({
+            code: "Ci.cSubDptCde"+rowId,
+            list: [{ label: '其他',value: '1',  }]
+          }
+      );
     }
     updateMasterAgreementValues()
   },
@@ -973,8 +976,8 @@ function setFormValue(value: any) {
         list:[{value:elem['Ci.cSlsCde'],label:`${elem['Ci.cSlsCde']}${elem['Ci.cSlsNme']}`}]
         })
         freeEditRef.value?.addCodeListMap({
-        code:"Ci.cSlsCde"+elem['_dataId'],
-        list:[{value:elem['Ci.cSlsCde'],label:`${elem['Ci.cSlsCde']}${elem['Ci.cSlsNme']}`}]
+          code:"Ci.cSlsCde"+elem['_dataId'],
+          list:[{value:elem['Ci.cSlsCde'],label:`${elem['Ci.cSlsCde']}${elem['Ci.cSlsNme']}`}]
         })
       }
     });
