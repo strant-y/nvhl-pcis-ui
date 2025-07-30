5bb2fe4b8aadf4a00b5e9c4bd6063dec2f23c990
yangyuhang <yhyangcd@isoftstone.com>
2025-07-30 10:40:59 +0800

    <dev_yyh>:发起风勘时间英文修改，文字提示不能展示null的字段

--- src/shared/com-item/rt-input.vue
+++ src/shared/com-item/rt-input.vue
@@ -1,7 +1,7 @@
 <template>
   <template v-if="!showLabel">
     <el-tooltip
-        :disabled="!changeContent && (!vInput || vInput === 'undefined' || vInput === '' || vInput === '0')"
+        :disabled="!changeContent && (!vInput || vInput === 'undefined' || vInput === '' || vInput === '0' || vInput === 'null')"
         placement="top"
     >
       <template #content>
