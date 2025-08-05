376e4a898f2f204fcede0a460f59e688ec529b1f
xq <xqke@isoftstone.com>
2025-08-05 10:11:21 +0800

    自定义录单 货运产品选择货物功能

--- src/pcis/prodRef/cvrgRef/trem-template.vue
+++ src/pcis/prodRef/cvrgRef/trem-template.vue
@@ -126,7 +126,10 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
+                  <tr
+                      :class="{'selected': isSelected(termdata)}"
+                      @click="selectRow(termdata)"
+                  >
                       <th v-if="checkExtendshow">
                         <a style="margin-right: 5px" @click="showExtend = !showExtend">
                           <el-icon v-if="!showExtend"><ArrowUpBold /></el-icon>
@@ -219,14 +222,15 @@
                 <tbody>
                   <template v-if="groupconf[ginfo.cGroupId]">
                     <template
-                      v-for="(riskdata, k, ri) in groupconf[ginfo.cGroupId]
-                        .riskList"
+                      v-for="(riskdata, k, ri) in groupconf[ginfo.cGroupId].riskList"
                       :key="k"
                     >
                       <template v-if="riskdata.maxNum > 0">
                         <tr
                           v-for="n in riskdata.maxNum"
                           :key="`${ginfo.cGroupId}-${k}-${n}`"
+                          :class="{'selected': isSelected(k)}"
+                          @click="selectRow(k)"
                         >
                           <template
                             v-for="colinfo in riskdata.col"
@@ -363,6 +367,8 @@ import { dataOpertaor } from "@/store/modules/data-opertaor";
 import { terConfig } from "@/store/modules/term-config";
 import { useValidator } from "@/typings/useValidator";
 import { useRoute } from "vue-router";
+import { v4 as uuidv4 } from "uuid";
+import {CommonConstants} from "@/constants/CommonConstants";
 const route = useRoute();
 const templateRef = ref();
 const opertaor = dataOpertaor();
@@ -396,6 +402,8 @@ const btnItem = ref<{ [key: string]: { [key: string]: any } }>({
     label: "删除",
   },
 });
+
+const {selectedRow} = storeToRefs(terconfig);
 
 function update() {
   let newData;
@@ -1184,7 +1192,7 @@ const methodMap = {
   }
 };
 
-const checkData = (v :nay,item:any) => {
+const checkData = (v :any,item:any) => {
     const cf = groupconf.value[item.cGroupId]['riskList'][item.cRiskNo]['rowConfig'][item.cColId];
     if(cf){
       const fk = item.prop
@@ -1211,6 +1219,23 @@ function setCancel(){
   termdata.value['Term.cCancelMrk'] = '1';
   update();
 }
+
+const isSelected = (key: any) => {
+  if(typeof key === CommonConstants.TYPE_OF_STRING) {
+    const row = riskList.value[key];
+    return selectedRow.value?._dataId === row._dataId;
+  }else {
+    if(!selectedRow.value) return false;
+    return key['Term.cClauseCode'] === selectedRow.value['Term.cClauseCode'];
+  }
+};
+const selectRow = (key: any) => {
+  if(typeof key === CommonConstants.TYPE_OF_STRING){
+    selectedRow.value = riskList.value[key];
+  } else {
+    selectedRow.value = key;
+  }
+};
 
 defineExpose({
   dataFlash,
@@ -1259,5 +1284,13 @@ td {
 }
 ::v-deep .el-form-item {
   margin-bottom: 0px !important; /* 使内容显示更近紧促 */
+}
+.table tr:hover {
+  background-color: #f5f5f5;
+  cursor: pointer;
+}
+
+.selected {
+  background-color: rgba(146, 209, 232, 0.5) !important;
 }
 </style>
