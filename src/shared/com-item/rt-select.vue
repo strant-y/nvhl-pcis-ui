831cf5feebd7852844e464cd81e56aacc2bfe41d
xq <xqke@isoftstone.com>
2025-08-08 10:27:45 +0800

    onUnmounted codelistMap clear

--- src/shared/com-item/rt-select.vue
+++ src/shared/com-item/rt-select.vue
@@ -436,6 +436,14 @@ function setChangeInfo(content: any) {
     changeContent.value = undefined;
   }
 }
+
+onUnmounted(() => {
+  if(Object.keys(codeListMap).length > 0) {
+    const rowId = props.row && props.row._dataId ? props.row._dataId : '';
+    delete codeListMap[props.item.typeCode + rowId];
+    delete codeListMap[props.item.prop + rowId];
+  }
+});
 defineExpose({
   updateOption,
   setCustomClass,
