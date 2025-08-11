63a80d270e99bb918987b8fe3547a0aca60b4326
baidong <10203040>
2025-08-11 09:18:24 +0800

    保单查询，当申请单类型选择为"A/E"（投保/批改）时，投保日期（tAppTm）/批改申请日期(tEdrAppTm)字段设置默认值，时间范围是最近15天（从15天前到当前时间）。

--- src/views/comprehensive-query/common/PlyQuery.vue
+++ src/views/comprehensive-query/common/PlyQuery.vue
@@ -566,6 +566,10 @@ const formconfig1 = reactive<AppFreeEditConfig>(
                           if (item.prop === "tAppTm") {
                               item.hidden = false; // 显示投保日期
                               item.rules = [getRules("required", {})]; // 设置必填规则
+                              // 设置默认值为最近15天
+                              const endDate = moment(new Date()).format("YYYY-MM-DD 23:59:59");
+                              const startDate = moment(new Date()).subtract(15, "days").format("YYYY-MM-DD 00:00:00");
+                              freeEditRef.value?.setValue("tAppTm", [startDate, endDate]);
                           } else if (item.prop == "tIssueTm" || item.prop == "tEdrAppTm") {
                               item.hidden = true;
                           }
@@ -576,6 +580,10 @@ const formconfig1 = reactive<AppFreeEditConfig>(
                           if (item.prop === "tEdrAppTm") {
                               item.hidden = false; // 显示批改申请日期
                               item.rules = [getRules("required", {})]; // 设置必填规则
+                            // 设置默认值为最近15天
+                            const endDate = moment(new Date()).format("YYYY-MM-DD 23:59:59");
+                            const startDate = moment(new Date()).subtract(15, "days").format("YYYY-MM-DD 00:00:00");
+                            freeEditRef.value?.setValue("tEdrAppTm", [startDate, endDate]);
                           } else if (item.prop == "tAppTm") {
                               item.hidden = true;
                           } else if (item.prop == "tIssueTm") {
