602200853664cce922bc140d20a5959f2fd723fc
zhangmingjie\YAIC <mjzhangcf@isoftstone.com>
2025-08-12 11:46:28 +0800

    非涉费组合批改生成批文时不需要验证账户信息

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -3501,10 +3501,13 @@ const saveEdrPlyInfo = async () => {
  * **/
 const generateEndorse = async () => {
   const res = opertaor.getDataAll();
-  const isAcctValid = await validateAcctinfo();
-  // 账户信息校验
-  if (!isAcctValid) {
-    return; 
+  console.log("生成批文",res, props.param);
+  if (props.param.cRsnCde !== 'FZ') {
+    const isAcctValid = await validateAcctinfo();
+    // 账户信息校验
+    if (!isAcctValid) {
+      return; 
+    }
   }
   const btn = getBtn("btnCompare");
   btn.loading = true;
@@ -3545,6 +3548,7 @@ const generateEndorse = async () => {
         // 保费变化量 < 0 说明批改之后保费减少, 此时需要设置账户信息
         if ((!!prmVar && prmVar < 0 ) || refundInsurance ) {
             const acctinfoRef =opertaor.getTableRefs()["acctinfo"];
+            console.log("账户信息", acctinfoRef);
              let acctinfoValidate =await opertaor.getTableRefByKey('acctinfo')?.validate()  // 账户信息 必填校验
               let isAcctinfo = isDetailCde(); // 是否有账户信息
                 if(!acctinfoValidate && isAcctinfo) {
