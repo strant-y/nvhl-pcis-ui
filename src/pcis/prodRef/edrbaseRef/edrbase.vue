679e8759a7d45dc2ba726e218e57034c1ac10e7d
wangxiran <xrwangda@isoftstone.com>
2025-08-15 10:20:28 +0800

    fix: #5855 UAT- 040003-全单退保-暂存后手续费比例未回显

--- src/pcis/prodRef/edrbaseRef/edrbase.vue
+++ src/pcis/prodRef/edrbaseRef/edrbase.vue
@@ -90,7 +90,7 @@ const formconfig1 = reactive<AppFreeEditConfig>(
         },
       },
       {
-        prop: "EdrBase.NSurrRate",
+        prop: "EdrBase.nSurrRate",
         inputtype: "rtnumber",
         title: "手续费比例",
         clearable: true,
@@ -99,7 +99,7 @@ const formconfig1 = reactive<AppFreeEditConfig>(
           if(v){
             if(v<0 || v>1){
               ElMessage.warning("批改信息的手续费比例 必须为0~1.0之间!")
-              setValue('EdrBase.NSurrRate',0)
+              setValue('EdrBase.nSurrRate',0)
             }
           }
         },
