c2ad61135ea2a0f3a535e4a4a0239c5f6becb493
zhangmingjie\YAIC <mjzhangcf@isoftstone.com>
2025-08-18 20:16:57 +0800

    产品040005产品，清单新增如果录入身份证号，自动回填出生年月

--- src/common/dzmodel/distmodel/distAdd.vue
+++ src/common/dzmodel/distmodel/distAdd.vue
@@ -338,10 +338,15 @@ onMounted(() => {
         item["hidden"] = true;
       }
     }
-
-        // 身份证类型自动回填年龄
+     // 身份证类型自动回填年龄
     if(item.prop =='Dist.cIdentificationNumber'){
       item['func'] = (val: string) => {
+        // 针对040005产品，自动回填出生年月
+        if(route.params.param.cProdNo === '040005' && val && val.length === 18) {
+          const birthDateFromId = val.substring(6, 14);
+          const formattedBirthDate = `${birthDateFromId.substring(0, 4)}-${birthDateFromId.substring(4, 6)}-${birthDateFromId.substring(6, 8)}`;
+          setValue('Dist.tBirthDate', formattedBirthDate);
+        }
         if(val && val.length === 18 && getValue('Dist.cDocumentType') === '120001') {
           const age = calculateAgeFromIdCard(val);
           setValue('Dist.nAge', age);
