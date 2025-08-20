383a5acbd8419074280cd70ca9b3d5f8baac62b1
wangxiran <xrwangda@isoftstone.com>
2025-08-20 12:11:07 +0800

    fix: 清单删除失败提示错误信息

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -426,8 +426,12 @@ const method = {
         ElMessage.success("删除成功");
         const queryParams = distTableRef.value?.getPartnerPage(false);
         method.handleQuery(queryParams, true);
+      } else {
+        ElMessage.error(res.msg);
       }
-    });
+    }).catch((err:any) => {
+      ElMessage.error(err.msg);
+    })
   },
   // 投保座位总数
   nSeatCapacityChange:(val:any)=>{
@@ -1012,8 +1016,12 @@ const method = {
           ElMessage.success("删除成功");
           const queryParams = distTableRef.value?.getPartnerPage(false);
           method.handleQuery(queryParams, true);
+        } else {
+          ElMessage.error(res.msg);
         }
-      });
+      }).catch((err:any) => {
+        ElMessage.error(err.msg)
+      })
     });
   }
 };
