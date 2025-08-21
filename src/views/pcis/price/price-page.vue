9b14709eefa1811ec26710f6c4d395eefe69fe28
ChenZhenghui <zhchenq@isoftstone.com>
2025-08-21 09:34:03 +0800

    1.解决询价单,投保单录入,刷新地址错误的问题

--- src/views/pcis/price/price-page.vue
+++ src/views/pcis/price/price-page.vue
@@ -3273,7 +3273,7 @@ const savePlyInfo = async () => {
             sessionStorage.setItem('needCalcValue', JSON.stringify(needCalc.value))
             const data = res.data?.result[0];
             router.replace({
-              path: "/pcis/my-page",
+              path: "/pcisapp/pricePage",
               query: {
                 param: JSON.stringify({
                   ...data,
