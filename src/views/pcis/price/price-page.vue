aec42cb2dfac3c34f279325f3073ecfeda21ad53
wangxiran <xrwangda@isoftstone.com>
2025-08-25 09:51:51 +0800

    fix: 修复询价出单页面进来报错

--- src/views/pcis/price/price-page.vue
+++ src/views/pcis/price/price-page.vue
@@ -2764,6 +2764,21 @@ function getFKFunc() {
   // return true;
 }
 
+function checkShow(k:any){
+  let r = true;
+  if(k.pageKey !== 'acctinfo'){
+    if(['ciMasterAgreement',
+        'ci','ourCompanyCiShare',
+      ].includes(k.pageKey)){  // 联共保组件,通过isCiJiMrk 控制
+        r = isCiJiMrk.value;
+    }else if (k.pageKey === 'distSummary') {  //隐藏全部汇总清单组件
+      r = false;
+    }else{
+      r = acctinfoFlag.value;
+    }
+  }
+  return r;
+}
 
 /**
  * 投保申请核保
@@ -2805,22 +2820,6 @@ const submitToUndrFn = async () => {
       }
     }
   }
-
-  function checkShow(k:any){
-  let r = true;
-  if(k.pageKey !== 'acctinfo'){
-    if(['ciMasterAgreement',
-        'ci','ourCompanyCiShare',
-       ].includes(k.pageKey)){  // 联共保组件,通过isCiJiMrk 控制
-        r = isCiJiMrk.value;
-    }else if (k.pageKey === 'distSummary') {  //隐藏全部汇总清单组件
-      r = false;
-    }else{
-      r = acctinfoFlag.value;
-    }
-  }
-  return r;
-}
 
 	// 申请核保前判断是否灰黑名单
 	const cInquiryNumber = opertaor.getTableRefByKey("plyBase").getValue("Base.cInquiryNo")
