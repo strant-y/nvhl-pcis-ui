8f6391bc4c1eea455ecda966a3d4e89c239f20c8
ChenZhenghui <zhchenq@isoftstone.com>
2025-08-27 12:05:26 +0800

    1.测试新增事故预防组件

---
+++ src/pcis/prodRef/accidentPrevRef/accidentPrev.vue
@@ -0,0 +1,122 @@
+<!-- 标的信息 -->
+<template>
+  <app-free-edit :freeEditConfig="formconfig1" ref="accidentPrevEditRef" />
+  <comDialog ref="dialog"></comDialog>
+</template>
+
+<script setup lang="ts">
+import {
+  AppFreeEditMethod,
+  createAppFreeEditConfig,
+} from "@/shared/app-free-edit-config";
+import { formInit } from "@/shared/from-init";
+import { dataOpertaor } from "@/store/modules/data-opertaor";
+import { useProductStore } from "@/store/modules/prod";
+import { useValidator } from "@/typings/useValidator";
+import { useRouter, useRoute } from 'vue-router';
+import { DialogMethod } from "@/common/dzmodel/ComDialogConf";
+import { getAddressStr } from "@/api/query";
+import { codeListViewStore } from "@/store";
+const codeListStore = codeListViewStore();
+import { distRequiredMap } from '@/views/pcis/my-page/requiredDistMap';
+
+const route = useRoute();
+const query = ref(route.query);
+const router = useRouter();
+
+const { getRules } = useValidator();
+const opertaor = dataOpertaor();
+const params = opertaor.getParam();
+const productStore = useProductStore()
+const dialog = ref<DialogMethod | null>(null);
+
+const props = defineProps({
+  pageSchema: {
+    type: [Object],
+    required: true,
+  },
+  compKey: {
+    type: String,
+    required: false,
+  },
+});
+onMounted(() => {
+  const formconfig11 = formInit(
+    JSON.stringify(props.pageSchema),
+    method,
+    exRules
+  );
+  Object.assign(formconfig1, formconfig11);
+});
+
+const accidentPrevEditRef = ref<AppFreeEditMethod | null>(null);
+
+const formconfig1 = reactive(createAppFreeEditConfig({}));
+
+// 绑定方法
+const method = {
+  
+  func1: () => {
+  },
+};
+
+// 绑定特殊验证器
+const exRules = {};
+function getFromValue() {
+  return tgtEditRef?.value?.getFromValue();
+}
+
+function setFormValue(value: any) {
+  tgtEditRef?.value?.setFormValue(value);
+}
+
+function validate() {
+  return tgtEditRef?.value?.validate();
+}
+
+function setValue(key: string, value: any) {
+  tgtEditRef?.value?.setValue(key, value);
+}
+
+function getValue(key: string) {
+  return tgtEditRef?.value?.getValue(key);
+}
+function clearValidate(key = null) {
+  tgtEditRef?.value?.clearValidate(key);
+}
+
+//给表单下拉项赋值
+function setFormItem(key: any, obj: any) {
+  if (obj && Object.keys(obj).length) {
+    formconfig1.fromSchema?.forEach((item) => {
+      if (item.prop === key) {
+        //控制尾部按钮的
+        if (item.btnItems && obj.btnItems) {
+          for (let key in obj.btnItems) {
+            item.btnItems[key] = obj.btnItems[key];
+          }
+        } else {
+          Object.assign(item, obj);
+        }
+      }
+    });
+  }
+}
+
+function getFormconfig() {
+  return formconfig1;
+}
+
+
+defineExpose({
+  getFromValue,
+  setFormValue,
+  validate,
+  setValue,
+  getValue,
+  getFormconfig,
+  clearValidate,
+});
+</script>
+
+<style scoped></style>
