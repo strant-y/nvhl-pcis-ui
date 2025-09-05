784bc156f60ac93b5553fdbfea5bceffe3903aeb
wangxiran <xrwangda@isoftstone.com>
2025-09-05 10:34:38 +0800

    fix: 040011-限制了出单机构必须是分公司对应的航保分中心，当前可使用其他机构复制成功

--- src/views/comprehensive-query/pages/classification-querys.vue
+++ src/views/comprehensive-query/pages/classification-querys.vue
@@ -149,6 +149,8 @@ const dzmodal = useDzModal();
 const tableRef = ref<AppTableMethod | null>(null);
 import DepartmentTree from "@/pcis/prodRef/commodityRef/DepartmentTree.vue";
 import {getAppPolicyList, getInquiryPolicyList, qryEndorseList, delTmpPolicy, queryInsuredList, getCustomUserList, CustomUserList, qryPolicyNewList} from "@/api/query";
+import { PolicyService } from '@/views/pcis-main/service/my-page/policy.service';
+const policyService = new PolicyService();
 // 变更列
 const colChange = defineAsyncComponent(() => import("../modal/colChange.vue"));
 const PrintView = defineAsyncComponent(() => import("../modal/PrintView.vue"));
@@ -1089,6 +1091,18 @@ const tableObj = {
                     console.log(row);
                     const r = await row;
                     if (r) {
+                        // 校验出单机构是否复合复制单的机构要求
+                        const queryProdDptCdeParam:any = { cDptCde:  user.value.companyId }
+                        if(row.cRenewMrk === "1") {
+                            queryProdDptCdeParam['cPlyNo'] = row.cPlyNo
+                        } else {
+                            queryProdDptCdeParam['cProdNo'] = row.cProdNo
+                        }
+                        const queryProdDptCde:any = await policyService.queryProdDptCde(queryProdDptCdeParam)
+                        if(queryProdDptCde.data !== true) {
+                            ElMessage.error(queryProdDptCde.msg)
+                            return
+                        }
                         row.cPolicySource = '8'
                         const data = row;
                         router.push({
