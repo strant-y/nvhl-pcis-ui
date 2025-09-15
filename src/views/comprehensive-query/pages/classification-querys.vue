75e8540cd7e8fb8a5230b06f26d9f256e8e2c858
zhangleDengzhou <723655642@qq.com>
2025-09-15 16:57:47 +0800

    综合查询-查询，批改展示批改申请单号/保单号

--- src/views/comprehensive-query/pages/classification-querys.vue
+++ src/views/comprehensive-query/pages/classification-querys.vue
@@ -11,14 +11,23 @@
       <!-- policyInfo 列的具名插槽 -->
        <template #column-policyInfo="{ row, column, index }">
         <div class="policy-info-cell">
-          <!-- 投保/批改-->
-          <template v-if="cAppType !== 'I'">
+          <!-- 投保 (申请单号/保单号)-->
+          <template v-if="cAppType == 'A'">
             <div v-if="row.cAppNo" class="policy-number-row">
             <span v-html="row.cAppNo"></span>
                 <el-icon class="copy-icon" @click="copyText(row.cAppNo)">
                     <DocumentCopy />
                 </el-icon>
             </div>
+            <div v-if="row.cPlyNo" class="policy-number-row">
+                <span v-html="row.cPlyNo"></span>
+                <el-icon class="copy-icon" @click="copyText(row.cPlyNo)">
+                   <DocumentCopy />
+                </el-icon>
+            </div>
+          </template>
+          <!-- 批改 (保单号)-->
+          <template v-else-if="cAppType == 'E'">
             <div v-if="row.cPlyNo" class="policy-number-row">
                 <span v-html="row.cPlyNo"></span>
                 <el-icon class="copy-icon" @click="copyText(row.cPlyNo)">
@@ -41,6 +50,32 @@
                 </el-icon>
             </div>
             </template>
+        </div>
+       </template>
+       <template #column-cEdrNo="{ row }">
+        <div class="policy-info-cell">
+          <template v-if="cAppType == 'A' || cAppType == 'I'">
+            <div v-if="row.cEdrNo" class="policy-number-row">
+                <span v-html="row.cEdrNo"></span>
+                <el-icon class="copy-icon" @click="copyText(row.cEdrNo)">
+                    <DocumentCopy />
+                </el-icon>
+            </div>
+          </template>
+          <template v-else>
+            <div v-if="row.cAppNo" class="policy-number-row">
+               <span v-html="row.cAppNo"></span>
+                <el-icon class="copy-icon" @click="copyText(row.cAppNo)">
+                    <DocumentCopy />
+                </el-icon>
+            </div>
+            <div v-if="row.cEdrNo" class="policy-number-row">
+                <span v-html="row.cEdrNo"></span>
+                <el-icon class="copy-icon" @click="copyText(row.cEdrNo)">
+                    <DocumentCopy />
+                </el-icon>
+            </div>
+          </template>
         </div>
       </template>
       <template #column-InsurancePeriod="{ row, column, index }">
@@ -78,9 +113,6 @@
         </el-tooltip>
       </template>
       <!-- ES查询 查询条件高亮 -->
-      <template #column-cEdrNo="{ row }">
-        <span v-html="row.cEdrNo || ''"></span>
-      </template>
       <template #column-cClntAddr="{ row }">
         <span v-html="row.cClntAddr || ''"></span>
       </template>
@@ -911,7 +943,6 @@ const normalQueryColumns = [
         prop: "cEdrNo",
         inputtype: "rtinput",
         title: "批单号",
-        minWidth: 180,
         slotName: "cEdrNo"
     },
     {
@@ -1930,6 +1961,8 @@ function updatePolicyInfoTitle(cAppTyp: string) {
   if (targetColumn) {
     if (cAppTyp === 'I') {
       targetColumn.title = '询价申请单号/询价单号';
+    } else if (cAppTyp === 'E') {
+      targetColumn.title = '保单号';
     } else {
       targetColumn.title = '申请单号/保单号';
     }
@@ -2073,11 +2106,12 @@ async function applyCheckedColumns(props: string[]) {
       const currentAppType = freeEditRef.value?.getFromValue()?.cAppTyp;
       if (currentAppType === 'I') {
         policyInfoCol.title = '询价申请单号/询价单号';
+      } else if (currentAppType === 'E') {
+        policyInfoCol.title = '保单号';
       } else {
         policyInfoCol.title = '申请单号/保单号';
       }
     }
-    
     const newConfig = {
         ...tableObj.notWaitObj,
         fromSchema: finalColumns
