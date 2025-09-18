7f0663c46fbccc5f951863d62f26722e877dd420
ChenZhenghui <zhchenq@isoftstone.com>
2025-09-18 16:44:35 +0800

    1.解决只读模式下,数据补全显示问题

--- src/pcis/prodRef/edrbaseRef/edrbase.vue
+++ src/pcis/prodRef/edrbaseRef/edrbase.vue
@@ -270,6 +270,7 @@ const formconfig1 = reactive<AppFreeEditConfig>(
           if(v === '99'){
             // 如果批改原因是99时,则直接修改自己的下拉选显示具体内容
             setFormItem("EdrBase.cEdrRsnBundleCde", { typeCode:null,loadData: [{ value: "99", label: "历史数据补全" }] });
+            setFormItem("EdrBase.cEdrRsnDetail", { loadData: [{value:'99',label:'历史数据补全'}] });
             setValue("EdrBase.cEdrRsnDetail", ['99']);
           }
         },
@@ -433,9 +434,7 @@ onMounted(() => {
         }
         setFormItem("EdrBase.cEdrRsnBundleCde", {typeCode:'', codeParam:'',loadData: [{value:'s1',label:'全单退保'},{value:'s2',label:'一般退保'},{value:'c1',label:'全单注销'}] });
     }else{
-      if(params['cRsnCde'] === '99'){
-        setFormItem("EdrBase.cEdrRsnDetail", { loadData: [{value:'99',label:'历史数据补全'}] });
-      }else{
+      if(params['cRsnCde'] !== '99'){
         const isGrp = params["cGrpMrk"] === "1" ? "1" : null;
         const isPer = params["CGrpMrk"] === "1" ? "1" : null;
         const param = {
