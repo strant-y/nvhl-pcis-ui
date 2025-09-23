f80fc80ae6c7c87728534555ba8e37c978c7c098
zhangleDengzhou <723655642@qq.com>
2025-09-23 11:17:43 +0800

    删除多余代码

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -2895,13 +2895,6 @@ const calcPremium = () => {
         })
       }
       opertaor.setDataAll(ops);
-    //   const whiteList = ['base', 'plyBase', 'applicant', 'insured', 'payinfo', 'EdrBase','insrnc'];
-    //   Object.keys(ops).forEach(key => {
-    //     if (whiteList.includes(key) && opertaor.getTableRefByKey(key)?.setFormValue) {
-    //         opertaor.getTableRefByKey(key).setFormValue(ops[key]);
-    //     }
-    //   });
-
       nPrm.value = ops["base"]["Base.nPrm"] ? ops["base"]["Base.nPrm"] : 0;
             nAmt.value = ops["base"]["Base.nAmt"] ? ops["base"]["Base.nAmt"] : 0;
 
