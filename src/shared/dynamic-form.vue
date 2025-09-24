e4cef4d01b2fd1daf1bc4e0975ed2a127e229969
xq <xqke@isoftstone.com>
2025-09-24 18:05:42 +0800

    表单分组标题上下边距调整

--- src/shared/dynamic-form.vue
+++ src/shared/dynamic-form.vue
@@ -883,8 +883,8 @@ defineExpose({
 </style>
 <style lang="css" scoped>
 .rt_group {
-   margin-top: 5px;
-   margin-bottom: 5px;
+   margin-top: 6px;
+   margin-bottom: 6px;
    background: var(--card-group-header-bg-color);
   /* border: 1px solid #D9D9D9; */
   padding: 0px 12px;
