59116badb3ae80a4d5388e87242dcee57d0c155b
wangxiran <xrwangda@isoftstone.com>
2025-09-25 10:01:38 +0800

    fix: 出单任务处理删除提示再保错误

--- src/views/pcis/workbenches/orderProcessing.vue
+++ src/views/pcis/workbenches/orderProcessing.vue
@@ -464,7 +464,7 @@ const tableconfig = reactive<AppTableConfig>(
               const param = {
                 cDocTyp: row.cAppTyp, // 单证类型 A 保单 E 批单
                 cAppNo: row.cAppNo, // 申请单号
-                cPlyNo: row.plyNo, // 保单号
+                cPlyNo: row.cPlyNo, // 保单号
                 nEdrPrjNo: row.nEdrPrjNo, // 批改序号
               };
               const delRisk =
