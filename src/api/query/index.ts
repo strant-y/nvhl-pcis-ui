021ec6a78d2317498c6b261038fbd0aeadc6086c
wangxiran <xrwangda@isoftstone.com>
2025-09-26 17:34:06 +0800

    fix: 查询页面历史数据修改点击查看页面调用接口

--- src/api/query/index.ts
+++ src/api/query/index.ts
@@ -79,7 +79,7 @@ export function qryEndorseList(queryParams: any): AxiosPromise<any> {
  */
 export function getAppPolicy(queryParams: any): AxiosPromise<any> {
     let url = '';
-    if ((!!queryParams['scene'] && 'EDR_APP_NEW_SCENE' === queryParams['scene']) || queryParams['cTransMrk'] === '1' ) {
+    if (!!queryParams['scene'] && 'EDR_APP_NEW_SCENE' === queryParams['scene']) {
         url = `/policy/getPolicy`;
     } else {
         url = `/policy/getAppPolicy`;
--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -12,7 +12,7 @@
           >
             <el-anchor :bound="120" :offset="80">
               <el-anchor-link
-                v-if="edrbaseFlag && (props.param.cRsnCde !== '99'|| props.param.cTransMrk !== '1')"
+                v-if="edrbaseFlag"
                 @click="handleAnchorClick($event, `#edrbase`)"
                 :class="activeAnchor === 'edrbase' ? 'isActive' : ''"
               >
@@ -32,7 +32,7 @@
                 >
               </el-anchor-link>
               <el-anchor-link
-                v-if="edritemFlag && (props.param.cRsnCde !== '99'|| props.param.cTransMrk == '1')"
+                v-if="edritemFlag"
                 @click="handleAnchorClick($event, `#edritem`)"
                 :class="activeAnchor === 'edritem' ? 'isActive' : ''"
               >
@@ -1355,10 +1355,10 @@ const initPage = async () => {
     //退保不显示产品组件信息
     acctinfoFlag.value = false;
   }
-  if(props.param.cRsnCde === '99' || props.param.cTransMrk === '1'){
-    edrbaseFlag.value = true
-    edritemFlag.value = true;
-  }
+  // if(props.param.cRsnCde === '99' || props.param.cTransMrk === '1'){
+  //   edrbaseFlag.value = true
+  //   edritemFlag.value = true;
+  // }
   // 页面初始化
   const formconfig11 = JSON.parse(getProductRes.data);
   oldProductResData.value = JSON.parse(getProductRes.data);
@@ -1819,7 +1819,7 @@ async function loadAfter() {
     }
   } else if (props.param.pageType === "readonly") {
     // 查询数据
-    const cAppNo = props.param?.cInquiryNo || props.param?.cAppNo;
+    const cAppNo = props.param.taskTyp === "I" ? props.param?.cInquiryNo : props.param?.cAppNo;
     await loadAppPlyInfo(cAppNo);
     if (props.param.cAppTyp == "E") {
       edritem.value?.handleQuery();
@@ -2588,12 +2588,10 @@ const loadAppPlyInfo = async (CAppNo) => {
     scene: props.param.pageType,
     cTransMrk:props.param.cTransMrk,
   };
-  if ("EDR_APP_NEW_SCENE" === props.param.pageType && props.param.cTransMrk != '1') {
+  if ("EDR_APP_NEW_SCENE" === props.param.pageType) {
     param["CPlyNo"] = CAppNo;
   } else if (props.param.pageName === "priceInquiry") {
     param["cInquiryNo"] = CAppNo;
-  } else if(props.param.cTransMrk === '1'){
-    param['CPlyNo'] = props.param.cAppNo;
   } else{
      param["cAppNo"] = CAppNo;
   }
@@ -2685,7 +2683,7 @@ const loadAppPlyInfo = async (CAppNo) => {
       });
     }
   } else {
-    const res = await getAppPolicy(param);
+    const res = await getAppPolicy(param, "getAppPolicy");
 
     console.log("投保单明细", res);
     if (res["code"] == "200") {
