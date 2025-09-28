f4f83536c3bc2d97822ff9ab2b2ce1b7161c40fd
wangxiran <xrwangda@isoftstone.com>
2025-09-28 21:50:19 +0800

    perf: 1.办理人员是个人，只展示个人的证件类型 2.投被保人性质是法人，这四个非必填，原来的必填逻辑注释掉 3.投被保人是法人，出生日期、年龄、性别、国籍、职业类别、经营范围、婚姻状况隐藏 投被保人是个人：是否分支机构、 股东客户隐藏 国籍默认中国

--- src/pcis/prodRef/applicantRef/applicant.vue
+++ src/pcis/prodRef/applicantRef/applicant.vue
@@ -90,6 +90,7 @@ onMounted(() => {
     //   rules: null,
     //   disabled: true,
     // });
+    setValue("Applicant.cNation", "CHN"); // 国籍默认中国
     //【国民经济行业分类】初始化必填，只有法人时才必填，现在个人也是必填了（老系统需求：040001/042002/043004/043005/043011五款产品不区分法人个人投保，国民经济行业分类都必填，其他产品只有法人才必填）
     const cProdNo = param.cProdNo;
     if (
@@ -522,6 +523,34 @@ const method = {
   InsureChange: (val) => {
     const param = opertaor.getParam(); ``
     if (val === "0") {
+      // 投保人是法人，出生日期、年龄、性别、国籍、职业类别、经营范围、婚姻状况隐藏
+      setFormItem("Applicant.tBirthday", {
+        hidden: true,
+      });
+      setFormItem("Applicant.nAge", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cSex", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cNation", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cOccupTyp", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cBusinessScope", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cMrgCde", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cIsBranch", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cStkMrk", {
+        hidden: false,
+      });
       setCapitalRequiredRule(getValue, setFormItem, 'Applicant');
       setFormItem("Applicant.tBirthday", {
         rules: null
@@ -537,17 +566,17 @@ const method = {
       clearValidate('Applicant.cSex')
 
       productStore.setcClntMrk(val);
-      // 办理人
-      setFormItem("Applicant.cCntrNme", { rules: [getRules("required", {})] });
-      setFormItem("Applicant.tOperaterCertfEndTm", {
-        rules: [getRules("required", {})],
-      });
-      setFormItem("Applicant.cOperaterCertfTyp", {
-        rules: [getRules("required", {})],
-      });
-      setFormItem("Applicant.cOperaterCertfCde", {
-        rules: [getRules("required", {})],
-      });
+      // 办理人(0928需求法人时办理人人员姓名、证件类型、证件号码必填、有效止期非必填)
+      // setFormItem("Applicant.cCntrNme", { rules: [getRules("required", {})] });
+      // setFormItem("Applicant.tOperaterCertfEndTm", {
+      //   rules: [getRules("required", {})],
+      // });
+      // setFormItem("Applicant.cOperaterCertfTyp", {
+      //   rules: [getRules("required", {})],
+      // });
+      // setFormItem("Applicant.cOperaterCertfCde", {
+      //   rules: [getRules("required", {})],
+      // });
       setFormItem("Applicant.cEnterpriseTel", {
         rules: [getRules("required", {})],
       });
@@ -669,6 +698,34 @@ const method = {
         rules: [],
       });
     } else {
+      setFormItem("Applicant.tBirthday", {
+        hidden: false,
+      });
+      setFormItem("Applicant.nAge", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cSex", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cNation", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cOccupTyp", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cBusinessScope", {
+        hidden: false,
+      });
+      setFormItem("Applicant.cMrgCde", {
+        hidden: false,
+      });
+      // 投保人是个人，是否分支机构、 股东客户隐藏
+      setFormItem("Applicant.cIsBranch", {
+        hidden: true,
+      });
+      setFormItem("Applicant.cStkMrk", {
+        hidden: true,
+      });
       setFormItem("Applicant.tBirthday", {
         rules: [getRules("required", {})],
       });
