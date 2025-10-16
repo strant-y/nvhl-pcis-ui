afea41e091ecf08fee63857cacf4378f937f54c6
wangxiran <xrwangda@isoftstone.com>
2025-10-16 20:31:50 +0800

    fix: 020009 增减保额批改时候这里的删除按钮多余  需要去掉

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -1245,6 +1245,18 @@ function getFatherPageOldProductResData() {
 function getFormConfig() {
   return tableconfig.value;
 }
+
+function setDisabledAll() {
+  tableconfig.value.formconfig?.endBtns?.forEach((item: any) => {
+    item.hidden = true;
+  });
+  tableconfig.value.formconfig?.titleBtns?.forEach((item: any) => {
+    item.hidden = true;
+  });
+  tableconfig.value.tableBtn?.forEach((item: any) => {
+    item.hidden = true;
+  });
+}
 defineExpose({
   getValue,
   setValue,
@@ -1256,6 +1268,7 @@ defineExpose({
   getTableData,
   setTableData,
   getFormConfig,
+  setDisabledAll,
 });
 </script>
 
