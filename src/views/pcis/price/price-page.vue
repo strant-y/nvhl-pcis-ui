8fafeb8c37b2bc5404595e8610d59a365b607398
wangxiran <xrwangda@isoftstone.com>
2025-10-24 16:20:24 +0800

    perf: 047001询价校验标的信息中的预估代驾人员数量(人)、预估代驾订单数量(单)二选一必填

--- src/views/pcis/price/price-page.vue
+++ src/views/pcis/price/price-page.vue
@@ -3466,6 +3466,10 @@ const submitToUndrFn = async () => {
       }
     }
 
+    if(!validateTgt()) {
+      return;
+    }
+
     //040005 校验 地址清单总数 和 学生人数（人）
     //    if(props.param.cProdNo ==='040005'){
     //         const isUnEqual = await checkStudentValidity();
@@ -5581,6 +5585,18 @@ const validateShanDong = async () => {
     return true;
   }
 };
+
+// 047001 校验标的信息中的预估代驾人员数量(人)、预估代驾订单数量(单)二选一必填
+const validateTgt = () => {
+  const tgtData = opertaor.getTableRefByKey("tgt").getFromValue();
+  if(props.param?.cProdNo === "047001") {
+    if(!tgtData['Tgt.nProxyDrivers'] && !tgtData['Tgt.nOrderQuantity']) {
+      ElMessage.error("标的信息预估代驾人员数量(人)、预估代驾订单数量(单)不能全部为空！")
+      return false;
+    }
+  }
+  return true;
+}
 
 opertaor.setFatherPage({
   currentIndex: currentIndex,
