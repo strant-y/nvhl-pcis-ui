baf5607ab6030b35d18f4658ac2b0377a6b0bab9
wangxiran <xrwangda@isoftstone.com>
2025-10-28 10:45:53 +0800

    fix: 山东见费出单校验不包含青岛

--- src/pcis/prodRef/baseRef/base.vue
+++ src/pcis/prodRef/baseRef/base.vue
@@ -747,7 +747,7 @@ const judgeShandongCase = async () => {
   const nPayNum = Number(baseData['Base.nPayNum'] || 0)  // "1"  缴费期数
 
   // 机构是山东分公司
-  if (!String(cDptCdeA).startsWith('0237')) return false;
+  if (!String(cDptCdeA).startsWith('023701')) return false;
   // 币种是人民币
   if (!['人民币', 'CNY'].includes(basePrm)) return false;
   // 联共保业务类型
--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -2890,7 +2890,7 @@ const calcPremium = () => {
         const cNeedfeeFlag = plyBase['Base.cNeedfeeFlag'];
 
         // 满足山东见费出单业务
-        if(cDptCde.startsWith('0237') && okProdPre.some(item => prod.startsWith(item)) 
+        if(cDptCde.startsWith('023701') && okProdPre.some(item => prod.startsWith(item)) 
         && !(['019904','089031'].includes(prod)) && !(['2','4','6'].includes(cCiMrk)) && (basePrm == "CNY") && (['0', '1'].includes(cClntMrk))){
             shanDongFlag = true;
             if (cClntMrk == '1' && (base['Base.cInstMrk'] == '5'|| cNeedfeeFlag == '0')){
@@ -5512,7 +5512,7 @@ const validateShanDong = async () => {
   const basePrm = baseData["Base.cPrmCur"]; //承保基本信息 总保费币种   // "CNY"
 
   // 机构是山东分公司
-  if (!String(cDptCdeA).startsWith('0237')) return false;
+  if (!String(cDptCdeA).startsWith('023701')) return false;
   // 币种是人民币
   if (!['人民币', 'CNY'].includes(basePrm)) return false;
   // 联共保业务类型
