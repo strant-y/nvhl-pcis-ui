8028eea1683c1c9cd9f3a124b27019a9f8ad9614
wangxiran <xrwangda@isoftstone.com>
2025-11-03 18:25:10 +0800

    perf: 是否修改累计赔偿限额选是则可以编辑修改后累计赔偿限额，且值不可以大于总保额，修改后累计赔偿限额值要同步共保总保额

--- src/pcis/prodRef/baseRef/base.vue
+++ src/pcis/prodRef/baseRef/base.vue
@@ -573,11 +573,25 @@ const method = {
       }
     }
   },
+  // 修改后总保额(累计赔偿限额)change事件
   aAmtChange: (val: any) => {
     const namtExch = getValue('Base.nAmtRmbExch');
     if (!!namtExch) {
       // 计算折人民币保额
       setValue("Base.nRmbAmt", numMulti(namtExch, val));
+    }
+    // 联共保主协议信息-共保总保额ciMasterAgreement
+    if(val && val > Number(getValue("Base.nCumulativeLimitModified") || 0)) {
+      ElMessage.warning("修改后总保额(累计赔偿限额)不能大于总保额(累计赔偿限额！");
+      setValue("Base.nAmt", getValue("Base.nCumulativeLimitModified"));
+      return;
+    }
+    const cCiMrkMap = ['1', '2', '3', '4'];
+    if(cCiMrkMap.includes(opertaor.getTableRefByKey('plyBase')?.getValue("Base.cCiMrk")) && getValue("Base.nCumulativeLimitModified")) {
+      opertaor.getTableRefByKey('ciMasterAgreement')?.setValue("Base.nCiJntAmt", val);
+      nextTick(() => {
+        ElMessage.warning("共保总保额发生变化，请重新计算保费！");
+      })
     }
   },
   // 总保额(累计赔偿限额)汇率change事件
@@ -590,20 +604,20 @@ const method = {
       }
     }
   },
-  // 是否修改每次事故赔偿限额 是和否change事件
+  // 是否修改累计赔偿限额 是和否change事件
   nAmtLimitManualChange: (val: any) => {
     if(val == '0'){
-        setFormItem("Base.nCumulativeLimitModified", { rules: [] });
+      setFormItem("Base.nAmt", { disabled: true });
     } else{
-        setFormItem("Base.nCumulativeLimitModified", { rules: [getRules("required", {})] });
+      setFormItem("Base.nAmt", { disabled: false });
     }
   },
-   // 是否修改累计赔偿限额
+   // 是否修改每次事故赔偿限额
   cAccidentLimitChange: (val: any) => {
     if(val == '0'){ // 否 
-        setFormItem("Base.nModifiedAccidentLimit", { rules: [] });
+        setFormItem("Base.nModifiedAccidentLimit", { rules: [], disabled: true });
     } else{  // 是 必填
-        setFormItem("Base.nModifiedAccidentLimit", { rules: [getRules("required", {})] });
+        setFormItem("Base.nModifiedAccidentLimit", { rules: [getRules("required", {})], disabled: false });
     }
   },
   // 总保费汇率change事件
