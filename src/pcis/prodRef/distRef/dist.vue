23fd26c86494617cc4565726175d369e28caaced
yangyuhang <yhyangcd@isoftstone.com>
2025-11-20 13:45:04 +0800

    <dev-yyh_1>:团单被保险人清单删除增加校验，修改营业场所地址清单关联被保险人value

--- src/pcis/prodRef/distRef/dist.vue
+++ src/pcis/prodRef/distRef/dist.vue
@@ -698,6 +698,7 @@ const method = {
 					const list = insuredDistData.length > 0 ? insuredDistData.map((i:any) => ({
 						label: i['InsuredDist.cInsuredNme'],
 						value: i['InsuredDist.cPkId']
+						// value: i['InsuredDist.cInsuredCde']
 					})) : []
 					eventBus.emit('setMap-AddressDist040001', {
 						code: 'Dist.cRelatedInsured',
@@ -1415,6 +1416,7 @@ function getFatherPageOldProductResData() {
           item.loadData = list.length > 0 ? list.map((i:any) => ({
             label: i['InsuredDist.cInsuredNme'],
             value: i['InsuredDist.cPkId']
+            // value: i['InsuredDist.cInsuredCde']
           })) : []
         }
       });
