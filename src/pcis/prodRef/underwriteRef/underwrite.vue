49cc1938e9e544267f86f19f6fa0379d2aec60ce
wangxiran <xrwangda@isoftstone.com>
2025-12-13 17:14:50 +0800

    fix: 核保时核保信息中的核保选项和核保意见选项保持原样，核保选项选择后自动设置核保意见选项默认值

--- src/pcis/prodRef/underwriteRef/underwrite.vue
+++ src/pcis/prodRef/underwriteRef/underwrite.vue
@@ -275,27 +275,85 @@ const formconfig1 = reactive<AppFreeEditConfig>(
         hidden: params.pageName === "priceInquiry"
       },
       {
+        prop: "cUndrMrk",
+        inputtype: "rtselect",
+        title: "核保选项",
+        loadData: [
+          {value: 'A', label: '同意'},
+          {value: 'R', label: '上报'},
+          {value: 'B', label: '退回给出单员'},
+          {value: 'T', label: '退回至指定核保级别人员'}
+        ],
+        rules: [getRules("required", {})],
+        clearable: true,
+        func: (v:any) => {
+          if ("A" === v) {
+            setValue("cUndrOpnList", "");
+            if ("E" === params["cAppTyp"]) {
+              setFormItem("cUndrOpnList", {
+                loadData: [{ label: "审核通过", value: "6" }],
+              });
+            } else {
+              setFormItem("cUndrOpnList", {
+                loadData: [{ label: "审核通过", value: "0" }],
+              });
+              setValue("cUndrOpnList", "0");
+            }
+          } else if ("R" === v) {
+            setValue("cUndrOpnList", "");
+            setFormItem("cUndrOpnList", {
+              loadData: [{ label: "提交上级", value: "5" }],
+            });
+            setValue("cUndrOpnList", "5");
+          } else {
+            setFormItem("cUndrOpnList", {
+              loadData: [
+                { label: "缺少必要信息", value: "1" },
+                { label: "修改承保条件", value: "2" },
+                { label: "费用超标", value: "3" },
+                { label: "拒绝承保", value: "4" },
+                { label: "录入错误", value: "7" },
+                { label: "资料不全", value: "8" },
+                { label: "其他", value: "9" },
+              ],
+            });
+            setValue("cUndrOpnList", "9");
+          }
+          if ("T" === v) {
+            setValue("cBckOp", "");
+            setFormItem("cBckOp", {
+              hidden: false,
+              rules: [getRules("required", {})],
+            });
+            const param = {
+              usrDptCde: user["companyId"],
+              operId: user["opCde"],
+              prodNo: params.cProdNo,
+              appNo: params.cAppNo,
+              dptCde: params.cDptCde,
+            };
+            console.log(param);
+            getBackClsListUrlFn(param);
+          } else {
+            setFormItem("cBckOp", {
+              hidden: true,
+              rules: "",
+            });
+            setValue("cBckOp", "");
+          }
+        },
+      },
+      {
         prop: "cUndrOpnList",
         inputtype: "rtselect",
         title: "核保意见选项",
-        loadData: params.pageName === "priceInquiry" ? [
+        loadData: [
           { label: "审核通过", value: "0" },
           { label: "缺少必要信息", value: "1" },
           { label: "修改承保条件", value: "2" },
           { label: "费用超标", value: "3" },
           { label: "拒绝承保", value: "4" },
-          { label: "提交上级", value: "5" },
-          { label: "录入错误", value: "7" },
-          { label: "资料不全", value: "8" },
-          { label: "其他", value: "9" },
-        ]:
-        [
-          { label: "审核通过", value: "E" === params["cAppTyp"] ? "6": "0" },
-          { label: "缺少必要信息", value: "1" },
-          { label: "修改承保条件", value: "2" },
-          { label: "费用超标", value: "3" },
-          { label: "拒绝承保", value: "4" },
-          { label: "提交上级", value: "5" },
+          { label: "其他", value: "5" },
         ],
         rules: [getRules("required", {})],
         itemWidth: 1,
@@ -354,9 +412,7 @@ const formconfig1 = reactive<AppFreeEditConfig>(
           n: "0",
         },
       },
-      {
-        itemWidth: 1
-      },
+
       {
         prop: "cIsRiskExp",
         inputtype: "rtselect",
@@ -383,81 +439,7 @@ const formconfig1 = reactive<AppFreeEditConfig>(
           }
         }
       },
-      {
-        itemWidth: 1,
-        hidden: params.pageName === "priceInquiry" ? true : false,
-      },
-      {
-        prop: "cUndrMrk",
-        inputtype: "rtselect",
-        title: "核保选项",
-        loadData: [
-          {value: 'A', label: '同意'},
-          {value: 'R', label: '上报'},
-          {value: 'B', label: '退回给出单员'},
-          {value: 'T', label: '退回至指定核保级别人员'}
-        ],
-        rules: [getRules("required", {})],
-        clearable: true,
-        func: (v) => {
-          // if ("A" === v) {
-          //   setValue("cUndrOpnList", "");
-          //   if ("E" === params["cAppTyp"]) {
-          //     setFormItem("cUndrOpnList", {
-          //       loadData: [{ label: "审核通过", value: "6" }],
-          //     });
-          //   } else {
-          //     setFormItem("cUndrOpnList", {
-          //       loadData: [{ label: "审核通过", value: "0" }],
-          //     });
-          //   }
-          // } else if ("R" === v) {
-          //   setValue("cUndrOpnList", "");
-          //   setFormItem("cUndrOpnList", {
-          //     loadData: [{ label: "提交上级", value: "5" }],
-          //   });
-          // } else {
-          //   setValue("cUndrOpnList", "");
-          //   setFormItem("cUndrOpnList", {
-          //     loadData: [
-          //       { label: "缺少必要信息", value: "1" },
-          //       { label: "修改承保条件", value: "2" },
-          //       { label: "费用超标", value: "3" },
-          //       { label: "拒绝承保", value: "4" },
-          //       { label: "录入错误", value: "7" },
-          //       { label: "资料不全", value: "8" },
-          //       { label: "其他", value: "9" },
-          //     ],
-          //   });
-          // }
-          
-          if ("T" === v) {
-            setValue("cBckOp", "");
-            setFormItem("cBckOp", {
-              hidden: false,
-              rules: [getRules("required", {})],
-            });
-            const param = {
-              usrDptCde: user["companyId"],
-              operId: user["opCde"],
-              prodNo: params.cProdNo,
-              appNo: params.cAppNo,
-              dptCde: params.cDptCde,
-            };
-            console.log(param);
-            getBackClsListUrlFn(param);
-          } else {
-            setFormItem("cBckOp", {
-              hidden: true,
-              rules: "",
-            });
-            setValue("cBckOp", "");
-          }
-        },
-      },
-      {
-        itemWidth: 1
-      },
+
       {
         prop: "cBckOp",
         inputtype: "rtselect",
@@ -523,6 +505,9 @@ function getBackClsListUrlFn(prarm) {
       setFormItem("cBckOp", {
         loadData: r["data"],
       });
+      if(r["data"]?.length === 1) {
+        setValue("cBckOp", r["data"][0]["value"]);
+      }
     }
   });
 }
