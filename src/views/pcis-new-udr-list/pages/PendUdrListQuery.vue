c0978c974f11003d02a565dd2658b098d069f0c1
wangxiran <xrwangda@isoftstone.com>
2025-12-15 21:57:45 +0800

    perf: 核保任务查询、询价任务查询列表列宽调整

--- src/views/pcis-new-udr-list/pages/PendUdrListQuery.vue
+++ src/views/pcis-new-udr-list/pages/PendUdrListQuery.vue
@@ -809,7 +809,7 @@ const tableconfig = reactive<AppTableConfig>(
         title: "承保机构",
         slotName: "cDptCnm",
         align: 'left',
-        lengthNum: 12,
+        lengthNum: 20,
       },
       {
         prop: "udrClsCde",
@@ -824,7 +824,7 @@ const tableconfig = reactive<AppTableConfig>(
         title: "条款名称",
         slotName: "cTermNme",
         align: 'left',
-        lengthNum: 13,
+        lengthNum: 25,
       },
       {
         prop: "cAppNme",
@@ -832,7 +832,7 @@ const tableconfig = reactive<AppTableConfig>(
         title: "投保人名称",
         slotName: "cAppNme",
         align: 'left',
-        lengthNum: 12,
+        // lengthNum: 12,
       },
       {
         prop: "cInsuredNme",
@@ -840,7 +840,7 @@ const tableconfig = reactive<AppTableConfig>(
         title: "被保人名称",
         slotName: "cInsuredNme",
         align: 'left',
-        lengthNum: 12,
+        // lengthNum: 12,
       },
       {
         prop: "nPrm",
@@ -868,7 +868,7 @@ const tableconfig = reactive<AppTableConfig>(
         inputtype: "rtinput",
         title: "任务提交人",
         align: 'left',
-        lengthNum: 5,
+        // lengthNum: 5,
       },
       // {
       //   prop: "cMinUndrCls",
