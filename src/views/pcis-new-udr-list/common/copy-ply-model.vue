906fbeedc4e06fbc11182f2258f7d24dc28ff1bd
wangxiran <xrwangda@isoftstone.com>
2025-12-23 10:09:15 +0800

    fix: 修复生产环境录单页面复制出单点击确认后接口提示缺少参数问题

--- src/views/pcis-new-udr-list/common/copy-ply-model.vue
+++ src/views/pcis-new-udr-list/common/copy-ply-model.vue
@@ -465,11 +465,9 @@ const confirm = async () => {
       return;
     }
     // 点击下一步前校验，如果data为true则继续，否则阻断并提示
-    const queryProdDptCdeParam:any = { cDptCde:  user.value.companyId }
+    const queryProdDptCdeParam:any = { cDptCde:  user.value.companyId, cProdNo: selected.value[0].cProdNo }
     if(selected.value[0].cRenewMrk === "1") {
         queryProdDptCdeParam['cPlyNo'] = selected.value[0].cPlyNo
-    } else {
-        queryProdDptCdeParam['cProdNo'] = selected.value[0].cProdNo
     }
     const queryProdDptCde:any = await policyService.queryProdDptCde(queryProdDptCdeParam)
     if(queryProdDptCde.data !== true) {
