bdf5331c7734112926de1ceeed1382c1a980dbec
wangxiran <xrwangda@isoftstone.com>
2025-12-29 20:03:34 +0800

    fix: 修复UAT-070002-复制出单-保险起期默认是今天的第二天，手动选择为今天的第三天； 页面会弹出该提示，但该产品无“起运日期”

--- src/pcis/prodRef/insrncRef/insrnc.vue
+++ src/pcis/prodRef/insrncRef/insrnc.vue
@@ -273,7 +273,7 @@ const method = {
       const timestamp1 = new Date(tDepartureDate).getTime();
       const timestamp2 = new Date(v).getTime();
       let cProdNo = route.params.param?.cProdNo;
-      if ((timestamp1 < timestamp2) && (cProdNo !== "020014" && cProdNo !== "020018")) {
+      if ((timestamp1 < timestamp2) && (cProdNo !== "020014" && cProdNo !== "020018" && cProdNo.startsWith('02'))) {
          ElMessage.warning("“起运日期”不能大于保险起期！");
         baseBefore["Base.tDepartureDate"] = ''
       }
