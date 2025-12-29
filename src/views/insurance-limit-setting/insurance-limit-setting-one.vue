29a3d2962a6ec8b42af60a95eb1265350b1b0883
wangxiran <xrwangda@isoftstone.com>
2025-12-29 21:32:32 +0800

    feat: 单一产品配置和全产品配置页面新增按钮增加通过接口判断是否展示

--- src/views/insurance-limit-setting/insurance-limit-setting-one.vue
+++ src/views/insurance-limit-setting/insurance-limit-setting-one.vue
@@ -246,9 +246,10 @@ const tableconfig = reactive<AppTableConfig>(
     showSelection: false,
     titleBtns: [
       createFreeButtonBase({
-        id: "score",
+        id: "add",
         label: "新增",
         type: "success",
+        hidden: true,
         func: function () {
           openEdit('save')
         },
@@ -586,6 +587,19 @@ onMounted(() => {
 	}, () => {
 		ElMessage.error('后台服务异常,请联系管理员');
 	});
+  // 获取新增按钮显示权限
+  insuranceLimit.jurisdictionInsert({}).then((res: any) => {
+    if (res.code == '1') {
+      const btn = tableconfig.titleBtns?.find(item => item.id === 'add');
+      if(btn) {
+        btn.hidden = false;
+      }
+    } else {
+      ElMessage.error(res.message);
+    }
+  }).catch((err:any) => {
+    ElMessage.error(err.message);
+  });
 });
 
 //给表单下拉项赋值
