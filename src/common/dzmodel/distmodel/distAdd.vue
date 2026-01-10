33a9e5b1d513e97d98369c3adaf139cb696a383b
yangyuhang <yhyangcd@isoftstone.com>
2026-01-10 14:17:20 +0800

    <uat-yyh_13>:清单信息证件类型编辑修改证件类型时才可以清空身份证号

--- src/common/dzmodel/distmodel/distAdd.vue
+++ src/common/dzmodel/distmodel/distAdd.vue
@@ -59,6 +59,8 @@ const props = defineProps({
   },
 });
 
+const init = ref(true)
+
 const cComponentTable = computed(() => props.data.compKey ? props.data.compKey.replace(/\d+/g, '') : "");
 
 const mapAddr = {
@@ -597,7 +599,10 @@ onMounted(() => {
   nextTick(() => {
     handelnInsuranceAmountList()
     // 同步dist组件中的codeListMap到表单中
-    freeEditRef.value?.setCodeListMap(props.data.codeListMap);
+		freeEditRef.value?.setCodeListMap(props.data.codeListMap);
+		setTimeout(() => {
+			init.value = false
+    }, 500);
   })
 });
 
@@ -760,7 +765,9 @@ const cEquipmentTypesFunc = ()=>{
 const cDocumentTypeChange = (val: any) => {
   const cIs = opertaor.getTableRefs()['tgt']?.getFromValue()['Tgt.cIsinsuranceRegistered']; // 是否记名投保
   const productNo = route.params?.param?.cProdNo; // 产品编号（兼容参数不存在的情况）
-	setValue('Dist.cIdentificationNumber', null);
+	if (!init.value) {
+		setValue('Dist.cIdentificationNumber', null);
+	}
 	clearValidate('Dist.cIdentificationNumber')
   const baseRuleMap: Record<string, any[]> = {
     "111": [getRules("idCard", {})], // 身份证
