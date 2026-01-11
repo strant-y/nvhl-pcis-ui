5f09452accf813a90a5fbeeb9207fbd367ea73d7
yangyuhang <yhyangcd@isoftstone.com>
2026-01-12 03:13:56 +0800

    <uat-yyh_12>:凭证类别只有在自定义、模板、复制出单时调用接口

--- src/pcis/prodRef/tgtRef/tgt.vue
+++ src/pcis/prodRef/tgtRef/tgt.vue
@@ -2008,7 +2008,10 @@ const method = {
   },
 	// 建设工程信息-保险凭证类别
 	cCertificateTypefun: async (val) => {
-		if (params.pageType != "app") {
+		if (params.initFlag) {
+			return false
+		}
+		if (params.pageType != "app" && params.pageType != "copy" && params.pageType != "template" ) {
 			return false
 		}
 		const cCertificateTypeProd = ["059011", "059012", "059013", "059015", "059016", "059017", "059018", "059019", "059020"]
