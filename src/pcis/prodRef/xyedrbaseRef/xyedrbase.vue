fab00c794ebb59f31773d33c082899283c705a18
yangyuhang <yhyangcd@isoftstone.com>
2026-01-29 20:28:33 +0800

    <uat-yyh_26>:批改过后的协议单进行查看，详情没有批改信息模块

--- src/pcis/prodRef/xyedrbaseRef/xyedrbase.vue
+++ src/pcis/prodRef/xyedrbaseRef/xyedrbase.vue
@@ -326,7 +326,7 @@ function setFormItem(key: any, obj: any) {
 onMounted(() => {
 	nextTick(() => {
 		// 协议审核批单展示批改信息和批改比较项，不能修改
-		if (params.type === "audit" && params.cAppTyp == 'E') {
+		if ((params.type === "audit" || params.type === "view") && params.cAppTyp == 'E') {
 			setFormItem("EdrECargoBase.cEdrMrk", { hidden: true })
 			setFormItem("EdrECargoBase.tEdrBgnTm", { disabled: true })
 			setFormItem("EdrECargoBase.cEdrCtnt", { disabled: true })
