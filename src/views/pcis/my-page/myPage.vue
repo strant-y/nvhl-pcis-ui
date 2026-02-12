36c763b992ae3d17e9b64b655df55e444e94f43e
yangyuhang <yhyangcd@isoftstone.com>
2026-02-12 10:14:28 +0800

    <uat-yyh_47>:批改反洗钱先保存

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -958,13 +958,20 @@ const setTaxInfo = () => {
  */
 const setCusBenefitInfo = (val) => {
   const tabref = opertaor.getTableRefs();
-  const appNo = tabref["applicant"]?.getFromValue()["Applicant.cAppNo"]; // 单据编号
   const AppcClntMrk = tabref["applicant"]?.getFromValue()["Applicant.cClntMrk"]; // 投保人 法人01
   const InscClntMrk = tabref["insured"]?.getFromValue()["Insured.cClntMrk"]; // 被保人  法人01
   const baseValue = opertaor.getTableRefByKey("base")?.getFromValue()["Base.nRmbPrm"];//承保基本信息 折合人民币总保费
   const basePrmCur = opertaor.getTableRefByKey("base")?.getFromValue()["Base.cPrmCur"];//承保基本信息 总保费币种
   const basePrm = opertaor.getTableRefByKey("base")?.getFromValue()["Base.nPrm"];//承保基本信息 总保费
 	let msg = val == 'view' ? '查看': '录入';
+
+
+	let appNo = null; // 单据编号
+	if (!!edrbase.value?.getFromValue()["EdrBase.cPlyNo"]) {
+			appNo = edrbase.value?.getFromValue()["EdrBase.cAppNo"];
+	} else {
+			appNo = opertaor.getTableRefByKey('plyBase')?.getValue('Base.cAppNo');
+	}
   //  单据保存才有 单据编号
   if (!appNo) {
     ElMessage.error("请先保存单据");
