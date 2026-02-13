08e3a47553b610f996d775ee7eea5b0d2896e362
yangyuhang <yhyangcd@isoftstone.com>
2026-02-13 15:35:27 +0800

    <uat-yyh_47>触发反洗钱提示词更改

--- src/views/pcis/my-page/myPage.vue
+++ src/views/pcis/my-page/myPage.vue
@@ -4254,7 +4254,7 @@ const validateNPrmAmlya = () => {
   }
   if(amlyaFlag()) {
     const CPrmCur = opertaor.getDataAll()["base"]['Base.cPrmCur']; // 保费币种
-    let msg = `根据反洗钱相关规定，当前保单保费大于等于${CPrmCur === 'USD' ? '1万美元' : '5万'}，请完善客户信息中：<br/>`;
+    let msg = `根据反洗钱相关规定，当前保单保费大于等于${CPrmCur === 'USD' ? '1万美元' : '5万'}时：`;
     let flag = false;
     
     // ----------投保人------------
@@ -4277,7 +4277,7 @@ const validateNPrmAmlya = () => {
         }
       }
       if (appMsg !== '') {
-        msg += `【投保人信息】${appMsg}<br/>`;
+				msg += '投保人信息中【' + appMsg.substring(0, appMsg.length - 1) + '】不能为空。';
       }
     }
     // ----------被保人------------
@@ -4307,12 +4307,11 @@ const validateNPrmAmlya = () => {
         }
       }
       if (insuredMsg !== '') {
-        msg += `【被保人信息】${insuredMsg}<br/>`;
+				msg += '被保人信息中【' + insuredMsg.substring(0, insuredMsg.length - 1) + '】不能为空。';
       }
     }
     if (flag) {
-      msg += '字段！'
-      ElMessage.error({message: msg, duration: 3000, dangerouslyUseHTMLString: true});
+      ElMessage.error(msg);
       return false;
     }
   } else {
@@ -4436,7 +4435,7 @@ const edrvalidateNPrmAmlya = (EdrBaseData) => {
 	if (edramlyaFlag(EdrBaseData)) {
 		nextTick(() => {
 			const CPrmCur = opertaor.getDataAll()["base"]['Base.cPrmCur']; // 保费币种
-			let msg = `根据反洗钱相关规定，当前批单保费变化大于等于${CPrmCur === 'USD' ? '1千美元' : '1万'}，请完善客户信息中：<br/>`;
+			let msg = `根据反洗钱相关规定，当前批单保费变化大于等于${CPrmCur === 'USD' ? '1千美元' : '1万'}时：`;
 			let flag = false;
 			let isDis = false;
 			let isMsg = false;
@@ -4458,48 +4457,43 @@ const edrvalidateNPrmAmlya = (EdrBaseData) => {
 				if(cAppNme != cAcctNme){
 					for (const i in edrexptArr) {
 						const objValue = edrexpDataVlue[edrexptArr[i]];
-						// if (objValue === null || objValue === '' || objValue === undefined) {
-							appMsg += edrexpCnmArr[i] + '、';
-							edrexp.value.setFormItem(edrexptArr[i], {
-								rules: [getRules("required", {})],
-								hidden: false,
-								disabled: !isDis
-							});
-							flag = true;
-						// }
-					}
-				} else {
-					const objValue = edrexpDataVlue['EdrBase.cSubtractPrmRsn'];
-					// if (objValue === null || objValue === '' || objValue === undefined) {
-						appMsg = '退保、减保或者办理保单贷款原因';
-						edrexp.value.setFormItem('EdrBase.cSubtractPrmRsn', {
+						appMsg += edrexpCnmArr[i] + '、';
+						edrexp.value.setFormItem(edrexptArr[i], {
 							rules: [getRules("required", {})],
 							hidden: false,
 							disabled: !isDis
 						});
-						edrexp.value.setFormItem('EdrBase.cNotBackAppRsn', {
-							rules: [],
-							hidden: false,
-							disabled: true
-						});
-						edrexp.value.setFormItem('EdrBase.cNotBackAppNo', {
-							rules: [],
-							hidden: false,
-							disabled: true
-						});
-						edrexp.value.setValue('EdrBase.cNotBackAppRsn', null)
-						edrexp.value.setValue('EdrBase.cNotBackAppNo', null)
 						flag = true;
-					// }
+					}
+				} else {
+					const objValue = edrexpDataVlue['EdrBase.cSubtractPrmRsn'];
+					appMsg = '退保、减保或者办理保单贷款原因、';
+					edrexp.value.setFormItem('EdrBase.cSubtractPrmRsn', {
+						rules: [getRules("required", {})],
+						hidden: false,
+						disabled: !isDis
+					});
+					edrexp.value.setFormItem('EdrBase.cNotBackAppRsn', {
+						rules: [],
+						hidden: false,
+						disabled: true
+					});
+					edrexp.value.setFormItem('EdrBase.cNotBackAppNo', {
+						rules: [],
+						hidden: false,
+						disabled: true
+					});
+					edrexp.value.setValue('EdrBase.cNotBackAppRsn', null)
+					edrexp.value.setValue('EdrBase.cNotBackAppNo', null)
+					flag = true;
 				}
 				
 				if (appMsg !== '') {
-					msg += `【批改扩展信息】${appMsg}<br/>`;
+					msg += '批改扩展信息中【' + appMsg.substring(0, appMsg.length - 1) + '】不能为空。';
 				}
 			}
 			if (flag && isMsg) {
-				msg += '字段！'
-				ElMessage.error({message: msg, duration: 3000, dangerouslyUseHTMLString: true});
+				ElMessage.error(msg);
 				return false;
 			}
 		})
