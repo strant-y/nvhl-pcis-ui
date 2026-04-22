0a67c97dca34072f56319e2555dc1ea27fb9b84a
xq <xqke@isoftstone.com>
2026-04-22 16:41:35 +0800

    组合出单-保费计算成员清单校验

--- src/views/pcis/composite/posite-page.vue
+++ src/views/pcis/composite/posite-page.vue
@@ -538,7 +538,7 @@ const calcPremium = async () => {
   if(props.param.cCombinationType === '2') {
     const o060030 = pageView.value.getDataOpertaorByProdNo('060030')
     const yjxGrpMemberList = o060030.getTableRefs()['yjxGrpMember'].getFormValue()
-    if(yjxGrpMemberList || yjxGrpMemberList.length !== 5) {
+    if(!yjxGrpMemberList || yjxGrpMemberList.length !== 5) {
       console.log('yjxGrpMemberList.length', yjxGrpMemberList.length)
       ElMessage.warning('团意险：团单成员清单必需录入5人')
       return;
