55533dc7311c029adf04efc6c2be63ce4290050f
ChenZhenghui <zhchenq@isoftstone.com>
2026-06-12 17:59:26 +0800

    1.解决个体工商户,必填的问题

--- src/pcis/prodRef/insuredRef/insured.vue
+++ src/pcis/prodRef/insuredRef/insured.vue
@@ -1074,31 +1074,31 @@ const method = {
   cIsIndvduBizChange: (val: any) => {
     if (val == "1") {
       setFormItem("Insured.cOccupCde", { rules: [getRules("required", {})] });
-      setFormItem("Insured.cOccupCde", { btnItems: { disabled: false } });
-      setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})] });
-      setFormItem("Insured.cTrdCde", { btnItems: { disabled: false } });
+      // setFormItem("Insured.cOccupCde", { btnItems: { disabled: false } });
+      // setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})] });
+      // setFormItem("Insured.cTrdCde", { btnItems: { disabled: false } });
     } else if (val == "0") {
       setFormItem("Insured.cOccupCde", { rules: [] });
-      setFormItem("Insured.cOccupCde", { btnItems: { disabled: true } });
-      setValue("Insured.cOccupCde", null);
+      // setFormItem("Insured.cOccupCde", { btnItems: { disabled: true } });
+      // setValue("Insured.cOccupCde", null);
 			//【国民经济行业分类】初始化必填，只有法人时才必填，现在个人也是必填了（老系统需求：040001/042002/043004/043005/043011五款产品不区分法人个人投保，国民经济行业分类都必填，其他产品只有法人才必填）
-			const cProdNo = opertaor.getParam()?.cProdNo;
-			if (
-				cProdNo === "040001" ||
-				cProdNo === "042002" ||
-				cProdNo === "043004" ||
-				cProdNo === "043005" ||
-				cProdNo === "043011"
-			) {
-				setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})],btnItems: { disabled: false } });
-			} else {
-				setFormItem("Insured.cTrdCde", { rules: [], btnItems: { disabled: true } });
-				setValue("Insured.cTrdCde", null);
-			}
+			// const cProdNo = opertaor.getParam()?.cProdNo;
+			// if (
+			// 	cProdNo === "040001" ||
+			// 	cProdNo === "042002" ||
+			// 	cProdNo === "043004" ||
+			// 	cProdNo === "043005" ||
+			// 	cProdNo === "043011"
+			// ) {
+			// 	setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})],btnItems: { disabled: false } });
+			// } else {
+			// 	setFormItem("Insured.cTrdCde", { rules: [], btnItems: { disabled: true } });
+			// 	setValue("Insured.cTrdCde", null);
+			// }
     } else {
-      setFormItem("Insured.cOccupCde", { btnItems: { disabled: false } });
-      setFormItem("Insured.cTrdCde", { btnItems: { disabled: false } });
-      setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})] })
+      // setFormItem("Insured.cOccupCde", { btnItems: { disabled: false } });
+      // setFormItem("Insured.cTrdCde", { btnItems: { disabled: false } });
+      // setFormItem("Insured.cTrdCde", { rules: [getRules("required", {})] })
     }
   },
   funcreset: () => {
